dfa0ce5841ddd4f6448172a2a5c314a27615ba42
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-07-16 20:23:12 -0700

    Time: 79 ms (73.50%) | Memory: 45.7 MB (71.22%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,52 +1,49 @@
 class Solution {
 public:
-    void DFS(int i, vector<vector<int>> &grph, vector<int> &vis)
+    int findpar(int X, vector<int> &parent)
     {
-        vis[i] = 1;
-        for(auto a : grph[i])
-        {
-            if(vis[a] == 0)
-            {
-                DFS(a, grph, vis);
-            }
-        }
+        if(X == parent[X]) return X;
 
-        return;
+        return parent[X] = findpar(parent[X], parent);
+    }
+
+    void join(int X, int Y, vector<int> &parent)
+    {
+        int upx = findpar(X, parent);
+        int upy = findpar(Y, parent);
+
+        if(upx == upy) return;
+
+        parent[upy] = upx;
     }
 
     int makeConnected(int n, vector<vector<int>>& connections) {
-        // determine number of coponents
-        // determine number of extra edges IN ALL components
-        // if(E >= components - 1) return components - 1 ans
-        // else -1
-
-        if(connections.size() < n - 1) return -1;
-
-        vector<vector<int>> grph(n);
-
-        for(int i = 0; i < connections.size(); i++)
-        {
-            int a = connections[i][0];
-            int b = connections[i][1];
-
-            grph[a].push_back(b);
-            grph[b].push_back(a);
-        }
-
-        vector<int> vis(n,0);
-
-        int count = 0;
+        vector<int> parent(n);
 
         for(int i = 0; i < n; i++)
         {
-            if(vis[i] == 0)
-            {
-                DFS(i, grph, vis);
-                count++;
-            }
+            parent[i] = i;
         }
 
-        if(connections.size() >= count - 1) return count - 1;
+        int extra = 0;
+        int components = n;
+        for(auto a : connections)
+        {
+            int b = a[0];
+            int c = a[1];
+
+            int upb = findpar(b, parent);
+            int upc = findpar(c, parent);
+
+            if(upb == upc) extra++;
+            else components--;
+
+            join(b, c, parent);
+        }
+
+        if(extra >= components - 1) return components - 1;
         else return -1;
-    }   
+
+
+    }
 };
