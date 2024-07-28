c082d811ad70d640d17c3cf198409665165ee10b
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-07-28 00:40:12 -0700

    Time: 186 ms (53.85%) | Memory: 8.3 MB (100.00%) - LeetSync

--- 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
@@ -1,43 +1,38 @@
 #include <cmath>
 #include <iostream>
-#include <vector>
 
 class Solution {
 public:
-    std::vector<bool> sieve(int maxN) {
-        std::vector<bool> isPrime(maxN + 1, true);
-        isPrime[0] = isPrime[1] = false;
-        for (int i = 2; i * i <= maxN; ++i) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= maxN; j += i) {
-                    isPrime[j] = false;
-                }
+    bool isPrime(int x) {
+        if (x <= 1) {
+            return false;
+        }
+        if (x == 2) {
+            return true;
+        }
+        if (x % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= std::sqrt(x); i += 2) {
+            if (x % i == 0) {
+                return false;
             }
         }
-        return isPrime;
+        return true;
+    }
+
+    int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime(i)) {
+                ++count;
+            }
+        }
+        return count;
     }
 
     int nonSpecialCount(int l, int r) {
-        int upper = std::ceil(std::sqrt(r));
-        int lower = std::floor(std::sqrt(l));
-
-        if (std::sqrt(l) != int(std::sqrt(l))) {
-            lower = lower + 1;
-        }
-
-        if (std::sqrt(r) != int(std::sqrt(r))) {
-            upper = upper - 1;
-        }
-
-        std::vector<bool> isPrime = sieve(upper);
-
-        int count = 0;
-        for (int i = lower; i <= upper; ++i) {
-            if (isPrime[i]) {
-                count++;
-            }
-        }
-
-        return r - l + 1 - count;
+        // x is special <=> x = p^2 for some prime p
+        return (r - l + 1) - countPrimes(std::sqrt(r) + 1) + countPrimes(std::sqrt(l - 1) + 1);
     }
 };
