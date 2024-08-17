df34e7f1158f880def296d8c6f39d5a61a1b7929
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-17 15:48:22 -0700

    Time: 196 ms (100.00%) | Memory: 182.9 MB (100.00%) - LeetSync

--- 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.cpp
+++ 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> result;
-        vector<int> temp;
+        if(k == 1) return nums;
 
-        for (int i = 0; i < n; i++) {
-            // If the window size is k, push the appropriate result and slide the window
-            if (temp.size() == k) {
-                result.push_back(temp.back());
-                temp.erase(temp.begin());
-            }
+        int x = 0;
 
-            // Check if the current number can be added to the temp (window)
-            if (temp.empty() || nums[i] == temp.back() + 1) {
-                temp.push_back(nums[i]);
-            } else {
-                // If not consecutive, clear the window and start fresh
-                temp.clear();
-                temp.push_back(nums[i]);
-            }
-
-            // If the window is full and elements are consecutive, push the max element
-            if (temp.size() == k) {
-                result.push_back(temp.back());
-                temp.erase(temp.begin());
-            } else if (temp.size() < k && i >= k - 1) {
-                // If the window is not full and we're past the initial elements, push -1
-                result.push_back(-1);
-            }
+        for(int i = 1; i < k; i++)
+        {
+            if(nums[i] != nums[i-1] + 1) x++;
         }
 
-        return result;
+        vector<int> ans;
+
+        if(x == 0)
+        {
+            ans.push_back(nums[k-1]);
+        }
+        else
+        {
+            ans.push_back(-1);
+        }
+
+        for(int i = k; i < n; i++)
+        {
+            if(nums[i] != nums[i-1] + 1) x++;
+
+            if(nums[i-k+1] != nums[i-k] + 1) x--;
+
+            if(x == 0) ans.push_back(nums[i]);
+            else ans.push_back(-1);
+        }
+
+        return ans;
     }
 };
-
