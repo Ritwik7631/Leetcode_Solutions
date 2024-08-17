ac86d43bba3f6efa0260d567be42d62f481e65d7
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-16 20:53:58 -0700

    Time: 19 ms (81.57%) | Memory: 26 MB (22.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    int fn(int i, int j, string &a, string &b, vector<vector<int>> &dp)
-    {
-        if(i >= a.size() || j >= b.size()) return 0;
+    // int fn(int i, int j, string &a, string &b, vector<vector<int>> &dp)
+    // {
+    //     if(i >= a.size() || j >= b.size()) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        if(a[i] == b[j])
-        {
-            return dp[i][j] = 1 + fn(i+1, j+1, a, b,dp);
-        }
+    //     if(a[i] == b[j])
+    //     {
+    //         return dp[i][j] = 1 + fn(i+1, j+1, a, b,dp);
+    //     }
 
-        return dp[i][j] = max(fn(i+1, j,a,b,dp), fn(i, j+1,a,b,dp));
-    }
+    //     return dp[i][j] = max(fn(i+1, j,a,b,dp), fn(i, j+1,a,b,dp));
+    // }
 
     int longestCommonSubsequence(string a, string b) {
         int i = 0;
         int j = 0;
 
-        vector<vector<int>> dp(a.size()+1, vector<int> (b.size() + 1, -1));
+        vector<vector<int>> dp(a.size()+1, vector<int> (b.size() + 1, 0));
 
-        return fn(i,j,a,b,dp);
+        for(int i = a.size()-1; i >= 0; i--)
+        {
+            for(int j = b.size()-1; j >= 0; j--)
+            {
+                if(a[i] == b[j])
+                {
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                    continue;
+                }
+
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);               
+            }
+        }
+
+        return dp[0][0];
     }
 };
