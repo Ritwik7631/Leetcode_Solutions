662cd8ed92ffc7ba105a8931cf766514d20268c1
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-16 18:04:58 -0700

    Time: 18 ms (92.32%) | Memory: 30.1 MB (19.27%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    int fn(int l, int r, string &s, vector<vector<int>> &dp)
-    {
-        if(l >= r) return 0;
+    // int fn(int l, int r, string &s, vector<vector<int>> &dp)
+    // {
+    //     if(l >= r) return 0;
 
-        if(dp[l][r] != -1) return dp[l][r];
+    //     if(dp[l][r] != -1) return dp[l][r];
 
-        if(s[l] == s[r])
-        {
-            return dp[l][r] = fn(l+1, r-1, s, dp);
-        }
+    //     if(s[l] == s[r])
+    //     {
+    //         return dp[l][r] = fn(l+1, r-1, s, dp);
+    //     }
         
-        return dp[l][r] = 1 + min(fn(l, r-1, s, dp), fn(l+1, r, s, dp));
-    }
+    //     return dp[l][r] = 1 + min(fn(l, r-1, s, dp), fn(l+1, r, s, dp));
+    // }
 
     int minInsertions(string s) {
-        int l = 0;
-        int r = s.size() - 1;
+        // int l = 0;
+        // int r = s.size() - 1;
 
-        vector<vector<int>> dp(s.size() + 1, vector<int> (s.size()+1, -1));
+        int n = s.size();
 
-        return fn(l, r, s, dp);
+        vector<vector<int>> dp(s.size()+1, vector<int> (s.size()+1, 0));
+
+        for(int l = n-1; l >= 0; l--)
+        {
+            for(int r = 0; r < n; r++)
+            {
+                if(l >= r) continue;
+
+                if(s[l] == s[r])
+                {
+                    dp[l][r] = dp[l+1][r-1];
+                    continue;
+                }
+                
+                dp[l][r] = 1 + min(dp[l][r-1], dp[l+1][r]);
+            }
+        }
+
+        return dp[0][n-1];
+
     }
 };
