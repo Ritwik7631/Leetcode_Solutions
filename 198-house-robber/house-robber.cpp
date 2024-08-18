4832f2deff9ddbed695c8c1ca6afb19351f3dfb2
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-18 12:00:08 -0700

    Time: 0 ms (100.00%) | Memory: 9.6 MB (8.87%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
-    int fn(int i, vector<int> &nums, vector<int> &dp)
-    {
-        if(i < 0) return 0;
-        if(i == 0)
-        {
-            return nums[i];
-        }
+    // int fn(int i, vector<int> &nums, vector<int> &dp)
+    // {
+    //     if(i < 0) return 0;
+    //     if(i == 0)
+    //     {
+    //         return nums[i];
+    //     }
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        int pick = nums[i] + fn(i-2, nums, dp);
-        int notpick = fn(i-1, nums, dp);
+    //     int pick = nums[i] + fn(i-2, nums, dp);
+    //     int notpick = fn(i-1, nums, dp);
 
-        return dp[i] = max(pick, notpick);
-    }
+    //     return dp[i] = max(pick, notpick);
+    // }
 
     int rob(vector<int>& nums) {
 
-        int n = nums.size();
-        vector<int> dp(n+1, -1);
-
-        return fn(n-1, nums, dp);
-
         // int n = nums.size();
+        // vector<int> dp(n+1, -1);
+
+        // return fn(n-1, nums, dp);
+// ----------------------------------------------------------------------
+        int n = nums.size();
         // if (n == 0) return 0;
-        // if (n == 1) return nums[0];
+        if (n == 1) return nums[0];
 
-        // vector<int> dp(n+1, 0);
+        vector<int> dp(n+1, 0);
 
-        // dp[0] = nums[0];
-        // dp[1] = max(nums[0], nums[1]);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
+        for(int i = 2; i < n; i++)
+        {
+            int pick = nums[i] + dp[i-2];
+            int notpick = dp[i-1];
+
+            dp[i] = max(pick, notpick);            
+        }
+
+        return dp[n-1];
+// -----------------------------------------------------------------------------------
         // int a = nums[0];
         // int b = max(nums[0], nums[1]);
         // int cur;
