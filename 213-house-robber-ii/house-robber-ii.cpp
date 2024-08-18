fe0425d64a6e0efb0bfb4ea7328370aba7c7090b
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-18 13:39:22 -0700

    Time: 4 ms (32.57%) | Memory: 9.9 MB (9.77%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,48 +1,66 @@
 class Solution {
 public:
-    int fn(int i, vector<int>& nums, vector<int>& dp)
-    {   
-        if(i < 0) return 0;
-        if(dp[i] != -1) return dp[i];
+    // int fn(int i, vector<int>& nums, vector<int>& dp)
+    // {   
+    //     if(i < 0) return 0;
+    //     if(dp[i] != -1) return dp[i];
 
-        int pick = nums[i];
-        if (i >= 2) {
-            pick += fn(i-2, nums, dp);
+    //     int pick = nums[i];
+    //     if (i >= 2) {
+    //         pick += fn(i-2, nums, dp);
+    //     }
+    //     int notpick = fn(i-1, nums, dp);
+
+    //     return dp[i] = max(pick, notpick);
+    // }
+
+    int bottomup(vector<int> nums)
+    {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++)
+        {
+            int pick = nums[i] + dp[i-2];
+            int notpick = dp[i-1];
+            dp[i] = max(pick, notpick);            
         }
-        int notpick = fn(i-1, nums, dp);
 
-        return dp[i] = max(pick, notpick);
+        return dp[n-1];
     }
 
     int rob(vector<int>& nums) {
 
-        int n = nums.size();
-        if (n == 1) return nums[0];
-
-        vector<int> nums1(nums.begin(), nums.end()-1);
-        vector<int> nums2(nums.begin() + 1, nums.end());
-
-        int a = nums1.size();
-        int b = nums2.size();
-
-        vector<int> dp(a, -1);
-        vector<int> dp2(b, -1);
-
-        return max(fn(a-1, nums1, dp), fn(b-1, nums2, dp2));
-
-// --------------------------------------------------------------
-
         // int n = nums.size();
-        // if(n == 1) return nums[0];
+        // if (n == 1) return nums[0];
 
         // vector<int> nums1(nums.begin(), nums.end()-1);
-        // vector<int> nums2(nums.begin()+1, nums.end());
+        // vector<int> nums2(nums.begin() + 1, nums.end());
 
         // int a = nums1.size();
         // int b = nums2.size();
 
-        // vector<int> dp(a, 0);
-        // vector<int> dp2(b, 0);
+        // vector<int> dp(a, -1);
+        // vector<int> dp2(b, -1);
 
+        // return max(fn(a-1, nums1, dp), fn(b-1, nums2, dp2));
+
+// --------------------------------------------------------------
+
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        vector<int> arr1(nums.begin() + 1, nums.end()); 
+        vector<int> arr2(nums.begin(), nums.end() - 1); 
+
+        int ans1 = bottomup(arr1);
+        int ans2 = bottomup(arr2);
+
+        return max(ans1, ans2);       
     }
 };
