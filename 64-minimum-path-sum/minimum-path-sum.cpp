310582598fed999c30dd6d151a5e28bc57e55d38
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-18 21:12:09 -0700

    Time: 3 ms (95.22%) | Memory: 12.9 MB (34.11%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,75 +1,75 @@
 class Solution {
 public:
     // the int fn returns to you the minimum sum from 0,0 to i,j
-    int fn(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
-    {
-        cout << i << " " << j << endl;
-        if(i == 0 && j == 0) return grid[0][0];
+    // int fn(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
+    // {
+    //     cout << i << " " << j << endl;
+    //     if(i == 0 && j == 0) return grid[0][0];
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int up = INT_MAX;
-        int left = INT_MAX;
+    //     int up = INT_MAX;
+    //     int left = INT_MAX;
 
-        if(i-1>=0)
-        {
-            up = grid[i][j] + fn(i-1,j,grid,dp);
-        }
+    //     if(i-1>=0)
+    //     {
+    //         up = grid[i][j] + fn(i-1,j,grid,dp);
+    //     }
 
-        if(j-1>=0)
-        {
-            left = grid[i][j] + fn(i,j-1,grid,dp);
-        }
+    //     if(j-1>=0)
+    //     {
+    //         left = grid[i][j] + fn(i,j-1,grid,dp);
+    //     }
 
-        return dp[i][j] = min(up, left);
-    }
+    //     return dp[i][j] = min(up, left);
+    // }
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return fn(n-1,m-1,grid, dp);
-// ----------------------------------------------------------------------------------------
-        // Bottom up
         // int n = grid.size();
         // int m = grid[0].size();
 
-        // vector<vector<int>> dp(n, vector<int> (m, 0));
+        // vector<vector<int>> dp(n, vector<int> (m, -1));
+        // return fn(n-1,m-1,grid, dp);
+// ----------------------------------------------------------------------------------------
+        // Bottom up
+        int n = grid.size();
+        int m = grid[0].size();
 
-        // dp[0][0] = grid[0][0];
+        vector<vector<int>> dp(n, vector<int> (m, 0));
 
-        // for(int i = 1; i < n; i++)
-        // {
-        //     dp[i][0] = grid[i][0] + dp[i-1][0];
-        // }
+        dp[0][0] = grid[0][0];
 
-        // for(int j = 1; j < m; j++)
-        // {
-        //     dp[0][j] = grid[0][j] + dp[0][j-1];
-        // }
+        for(int i = 1; i < n; i++)
+        {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
 
-        // for(int i = 1; i < n; i++)
-        // {
-        //     for(int j = 1; j < m; j++)
-        //     {
-        //         int up = INT_MAX;
-        //         int left = INT_MAX;
+        for(int j = 1; j < m; j++)
+        {
+            dp[0][j] = grid[0][j] + dp[0][j-1];
+        }
 
-        //         if(i-1>=0)
-        //         {
-        //             up = dp[i-1][j];
-        //         }
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
+            {
+                int up = INT_MAX;
+                int left = INT_MAX;
 
-        //         if(j-1>=0)
-        //         {
-        //             left = dp[i][j-1];
-        //         }
+                if(i-1>=0)
+                {
+                    up = dp[i-1][j];
+                }
 
-        //         dp[i][j] = min(up, left) + grid[i][j];    
-        //     }
-        // }
+                if(j-1>=0)
+                {
+                    left = dp[i][j-1];
+                }
 
-        // return dp[n-1][m-1];
+                dp[i][j] = min(up, left) + grid[i][j];    
+            }
+        }
+
+        return dp[n-1][m-1];
 
         // Space Optimized
 // ----------------------------------------------------------------------------
