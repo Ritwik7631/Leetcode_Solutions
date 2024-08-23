4607e81ee8ba8ea188dabc0207ccc8d2198a9537
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-23 02:49:35 -0700

    Time: 42 ms (49.45%) | Memory: 31.9 MB (13.87%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -24,28 +24,26 @@ public:
     // }
 
     bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size()+1, vector<int> (p.size()+1, 0));
+
         int n = s.size();
         int m = p.size();
 
-        // Initialize DP table
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        // Base case: Empty pattern matches with empty string
         dp[n][m] = 1;
 
-        // Fill the DP table
-        for (int i = n; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                bool match = (i < n && (s[i] == p[j] || p[j] == '?'));
-                if (p[j] == '*') {
-                    dp[i][j] = dp[i][j + 1] || (i < n && dp[i + 1][j]);
-                } else if (match) {
-                    dp[i][j] = dp[i + 1][j + 1];
-                } else {
-                    dp[i][j] = 0;
+        for(int i = n; i >= 0; i--)
+        {
+            for(int j = m-1; j >= 0; j--)
+            {
+                if(i<n && (s[i] == p[j] || p[j] == '?'))
+                {
+                    dp[i][j] = dp[i+1][j+1];
                 }
+                else if(p[j] == '*') dp[i][j] = dp[i][j+1] || (i < n && dp[i + 1][j]);
+                else dp[i][j] = false;
             }
         }
+
 
         return dp[0][0];
     }
