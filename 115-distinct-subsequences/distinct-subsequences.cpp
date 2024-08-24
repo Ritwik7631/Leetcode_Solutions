525e5ceca420615f8b4c12348fc8a3e1e2c3d51c
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-08-24 08:30:18 -0700

    Time: 24 ms (36.81%) | Memory: 20.1 MB (54.06%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -8,18 +8,49 @@ public:
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int count = 0;
         if(s[i] == t[j])
         {
-            count = fn(i-1, j-1, s, t, dp); 
+            return dp[i][j] = fn(i-1, j-1, s, t, dp) + fn(i-1, j, s, t, dp); 
         }
 
-        return dp[i][j] = count + fn(i-1, j, s, t, dp);
+        return dp[i][j] = fn(i-1, j, s, t, dp);
     }
+
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
         vector<vector<int>> dp(n, vector<int> (m, -1));
         return fn(n-1, m-1, s, t, dp);
+
+// -----------------------------------------------------------------------------------------------
+// bottom up
+
+        // int n = s.size();
+        // int m = t.size();
+
+        // vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+
+        // for(int i = 0; i <= n; i++)
+        // {
+        //     dp[i][0] = 1;
+        // }
+
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     for(int j = 1; j <= m; j++)
+        //     {
+        //         int count = 0;
+        //         if(s[i] == t[j])
+        //         {
+        //             count = dp[i-1][j-1]; 
+        //         }
+        //         else
+        //         {
+        //             dp[i][j] = count + dp[i-1][j];
+        //         }                
+        //     }
+        // }
+
+        // return dp[n-1][m-1];
     }
 };
