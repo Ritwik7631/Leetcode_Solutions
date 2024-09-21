ba9f25526b28f9cc863f85b67c92c9262bdd1540
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-09-21 12:57:23 -0700

    Time: 7 ms (88.97%) | Memory: 16.6 MB (11.79%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,71 +10,49 @@
  */
 class Solution {
 public:
-    void fn(ListNode* node, ListNode** newHead)
+    void fn(ListNode* curr, ListNode*& ans)
     {
-        if (node == nullptr || node->next == nullptr)
+        if(curr == nullptr || curr->next == nullptr)
         {
-            *newHead = node;
+            ans = curr;
             return;
         }
 
-        ListNode* nextNode = node->next;
-        fn(nextNode, newHead);
-        nextNode->next = node;
-        node->next = nullptr;
+        ListNode* nxt = curr->next;
+        fn(nxt, ans);
+        nxt->next = curr;
+        curr->next = nullptr;
     }
 
-
-    /*
-    fn(1,1)  
-        fn(2,1)
-            fn(3,1) 
-                fn(4,1) 
-                    fn(5,1) head = 5 
-    
-    
-    */
-
     ListNode* reverseKGroup(ListNode* head, int k) {
-        
-        ListNode* mover = head;
 
-        int counter = 0;
-        ListNode* b = nullptr;
         ListNode* a = nullptr;
+        ListNode* b = nullptr;
         ListNode* d = nullptr;
 
-        ListNode* anshead = nullptr;
+        ListNode* mover = head;
+        ListNode* ans = nullptr;
+        int counter = 0;
 
         while(mover != nullptr)
         {
             counter++;
-
-            cout << "counter: " << counter << endl;
 
             if(counter == 1) b = mover;
 
             if(counter == k)
             {
                 d = mover->next;
-                mover->next = nullptr;
+
+                mover->next = nullptr; // remember to update mover
 
                 ListNode* c = nullptr;
+    
+                fn(b, c);
 
-                fn(b, &c);
-
-                // if(prevhead != nullptr) cout << "prevhead: " << prevhead->val << '\n';
-                // else cout << "prev head: NULL" << '\n';
-
-                // if(temphead != nullptr) cout << "temphead: " << temphead->val << '\n';
-                // else cout << "temp head: NULL" << '\n';
-
-                // cout << "rv head: " << rvhead->val << endl;
-
-                // if(nxthead != nullptr) cout << "nxthead: " << nxthead->val << '\n';
-                // else cout << "nxt head: NULL" << '\n';
- 
-                if(anshead == nullptr) anshead = c;                
+                if (ans == nullptr) {
+                    ans = c;
+                }
 
                 if(a != nullptr) a->next = c;
 
@@ -87,14 +65,16 @@ public:
             else
             {
                 mover = mover->next;
-            }
+            } 
         }
 
-        if(counter > 0 && a != nullptr)
-        {
-            a->next = b;
+        if (counter > 0 && a != nullptr) {
+            a->next = b; // `b` is the start of the remaining nodes
         }
 
-        return anshead != nullptr ? anshead : head;
-    }   
+        return ans;
+
+
+
+    }
 };
