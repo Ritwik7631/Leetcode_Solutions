45842a760bab75fa6cafb673da198151d62f66eb
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-09-21 18:36:46 -0700

    Time: 3 ms (91.42%) | Memory: 14.6 MB (84.92%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,42 +17,53 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp;
-
         Node* mover = head;
-
-        Node* mover2 = nullptr;
 
         Node* headans = nullptr;
 
         while(mover != nullptr)
         {
-            if(mover == head)
-            {
-                mover2 = new Node(head->val);
-                headans = mover2;
-            }
-            else
-            {
-                mover2->next = new Node(mover->val);
-                mover2 = mover2->next;
-            }
-            mp[mover] = mover2;
-            mover = mover->next;
+            Node* curr = new Node(mover->val);
+            curr->next = mover->next;
+            mover->next = curr;
+
+            if(headans == nullptr) headans = curr;
+
+            mover = curr->next;
         }
 
+        // Node* tempmover = head;
+
+        // while(tempmover != nullptr)
+        // {
+        //     cout << tempmover->val << " ";
+        //     tempmover = tempmover->next;
+        // }
+
+        // original->next->random = original->random->next
+
         mover = head;
-        mover2 = headans;
 
         while(mover != nullptr)
         {
-            if(mover->random != nullptr) mover2->random = mp[mover->random];
+            if(mover->random != nullptr) mover->next->random = mover->random->next;
+            mover = mover->next->next;
+        }
+
+        mover = head;
+        Node* mover2 = headans;
+
+        while(mover != nullptr)
+        {
+            if(mover->next != nullptr) mover->next = mover->next->next;
+
+            if(mover2->next != nullptr) mover2->next = mover2->next->next;
 
             mover = mover->next;
+
             mover2 = mover2->next;
         }
 
         return headans;
-
     }
 };
