621305d051f0640b5418c065c49af63980e2037e
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-09-24 13:25:46 -0700

    Time: 15 ms (72.69%) | Memory: 18.7 MB (26.91%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    bool DFS(int i, vector<int> &vis, vector<int> &path_vis, vector<vector<int>> &adj)
+    {
+        vis[i] = 1;
+        path_vis[i] = 1;
+        bool flag = false;
+        for(auto a : adj[i])
+        {
+            if(vis[a] == 0)
+            {
+                if(path_vis[a] == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    flag = flag || DFS(a, vis, path_vis, adj);
+                }
+            }
+            else
+            {
+                if(path_vis[a] == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        path_vis[i] = 0;
+
+        return flag;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& x) {
+        int n = numCourses;
+        vector<int> vis(n, 0);
+        vector<int> path_vis(n, 0);
+
+        vector<vector<int>> adj(n);
+
+        for(int i = 0; i < x.size(); i++)
+        {
+            int a = x[i][0];
+            int b = x[i][1];
+
+            adj[b].push_back(a);
+        }
+
+        bool flag = false;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(vis[i] == 0)
+            {
+                flag = flag || DFS(i, vis, path_vis, adj); 
+                // Think of DFS to see whether or not a cycle exists
+            }
+        }
+
+        return !flag;
+    }
+};
