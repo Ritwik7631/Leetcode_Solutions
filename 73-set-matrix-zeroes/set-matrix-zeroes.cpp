676e23483f432ebbba6cff5d70c8d52249975175
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-09-24 18:30:07 -0700

    Time: 5 ms (89.32%) | Memory: 16.9 MB (78.25%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -24,18 +24,7 @@ public:
         {
             for(int i = 0; i < n; i++)
             {
-                if(row[i] == 1)
-                {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(col[j] == 1)
+                if(row[i] == 1 || col[j])
                 {
                     matrix[i][j] = 0;
                 }
