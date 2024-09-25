de370a9b5c15fd991053cca4cb1bfa330f39748e
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-09-24 20:10:35 -0700

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,36 +1,63 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>> ans;
+        // vector<vector<int>> ans;
 
-        vector<vector<long long>> grid(n, vector<long long> (n, 1));
+        // vector<vector<long long>> grid(n, vector<long long> (n, 1));
 
-        for(int i = 1; i < n; i++)
+        // for(int i = 1; i < n; i++)
+        // {
+        //     for(int j = 1; j < n; j++)
+        //     {
+        //         grid[i][j] = grid[i-1][j] + grid[i][j-1];
+        //     }
+        // }
+
+        // for(int i = 0; i < n; i++)
+        // {
+        //     int j = 0;
+
+        //     vector<int> temp;
+
+        //     int x = i;
+
+        //     while(x >= 0 && j <= i)
+        //     {
+        //         temp.push_back(grid[x][j]);
+        //         x--;
+        //         j++;
+        //     }
+
+        //     ans.push_back(temp);
+        // }
+
+        // return ans;
+
+// ---------------------------------------------
+
+    vector<vector<int>> ans;
+
+    ans.push_back({1});
+
+    if(n == 1) return ans;
+
+    ans.push_back({1,1});
+
+    if(n == 2) return ans;
+
+    for(int i = 3; i <= n; i++)
+    {
+        vector<int> temp;
+        temp.push_back(1);
+        for(int j = 1; j < ans[i-2].size(); j++)
         {
-            for(int j = 1; j < n; j++)
-            {
-                grid[i][j] = grid[i-1][j] + grid[i][j-1];
-            }
+            temp.push_back(ans[i-2][j] + ans[i-2][j-1]);
         }
+        temp.push_back(1);
+        ans.push_back(temp);
+    }
 
-        for(int i = 0; i < n; i++)
-        {
-            int j = 0;
+    return ans;
 
-            vector<int> temp;
-
-            int x = i;
-
-            while(x >= 0 && j <= i)
-            {
-                temp.push_back(grid[x][j]);
-                x--;
-                j++;
-            }
-
-            ans.push_back(temp);
-        }
-
-        return ans;
     }
 };
