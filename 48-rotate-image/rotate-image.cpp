769166d12d813bc759a05378b6eadbfeb8381314
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-03 13:34:37 -0700

    Time: 4 ms (39.56%) | Memory: 8.9 MB (84.27%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,22 +3,20 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        for (int i = 0; i < n / 2; i++) {
-            for (int j = i; j < n - 1 - i; j++) {
-                // Store the four elements involved in the rotation
-                int temp = matrix[i][j];
-                
-                // Move values from left to top
-                matrix[i][j] = matrix[n - 1 - j][i];
-                
-                // Move values from bottom to left
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
-                
-                // Move values from right to bottom
-                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-                
-                // Assign temp (top) to right
-                matrix[j][n - 1 - i] = temp;
+        for(int i = 0; i < (n+1)/2; i++) {
+            for(int j = i; j < n - 1 - i; j++) {
+                // Define the four points that need to be swapped
+                int x1 = i, y1 = j;
+                int x2 = j, y2 = n - 1 - i;
+                int x3 = n - 1 - i, y3 = n - 1 - j;
+                int x4 = n - 1 - j, y4 = i;
+
+                // Perform the four-way swap
+                int temp = matrix[x1][y1];
+                matrix[x1][y1] = matrix[x4][y4];
+                matrix[x4][y4] = matrix[x3][y3];
+                matrix[x3][y3] = matrix[x2][y2];
+                matrix[x2][y2] = temp;
             }
         }
     }
