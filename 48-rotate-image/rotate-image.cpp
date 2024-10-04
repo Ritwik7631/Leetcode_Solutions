6613de1261a92588fff85c94e9845645b6cb396c
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-03 18:12:13 -0700

    Time: 2 ms (47.82%) | Memory: 8.8 MB (96.78%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,21 +3,27 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        for(int i = 0; i < (n+1)/2; i++) {
-            for(int j = i; j < n - 1 - i; j++) {
-                // Define the four points that need to be swapped
-                int x1 = i, y1 = j;
-                int x2 = j, y2 = n - 1 - i;
-                int x3 = n - 1 - i, y3 = n - 1 - j;
-                int x4 = n - 1 - j, y4 = i;
+        for(int i = 0; i < (n+1)/2; i++)
+        {
+            pair<int,int> p1 = {i,i};
+            pair<int,int> p2 = {i, n-1-i};
+            pair<int,int> p3 = {n-1-i, n-1-i};
+            pair<int,int> p4 = {n-1-i,i};
 
-                // Perform the four-way swap
-                int temp = matrix[x1][y1];
-                matrix[x1][y1] = matrix[x4][y4];
-                matrix[x4][y4] = matrix[x3][y3];
-                matrix[x3][y3] = matrix[x2][y2];
-                matrix[x2][y2] = temp;
+            for(int j = p1.second; j < p2.second; j++)
+            {
+                swap(matrix[p1.first][p1.second], matrix[p2.first][p2.second]);
+                swap(matrix[p1.first][p1.second], matrix[p3.first][p3.second]);
+                swap(matrix[p1.first][p1.second], matrix[p4.first][p4.second]);
+
+                p1.second = p1.second + 1;
+                p2.first = p2.first + 1;
+                p3.second = p3.second - 1;
+                p4.first = p4.first - 1;
             }
         }
+
+        return;
+        
     }
 };
