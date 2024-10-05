09c70876a3f07828cf36773a2fe504db02d57648
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-04 19:54:51 -0700

    Time: 0 ms (100.00%) | Memory: 12.2 MB (41.45%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,20 +1,48 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+
+        // for(int i = n-1; i >= 0; i--)
+        // {   
+        //     if(matrix[i][m-1] < target) continue;
+
+        //     if(matrix[i][m-1] == target) return true;
+
+        //     for(int j = m-1; j >= 0; j--)
+        //     {
+        //         if(matrix[i][j] == target) return true;
+        //     }
+        // }
+
+        // return false;
+
+// --------------------------------------------------------------------------------
         int n = matrix.size();
         int m = matrix[0].size();
 
-        for(int i = n-1; i >= 0; i--)
-        {   
-            if(matrix[i][m-1] < target) continue;
+        int l = 0;
+        int r = n*m;
 
-            if(matrix[i][m-1] == target) return true;
+        while(l < r)
+        {
+            int mid = (r-l)/2 + l;
 
-            for(int j = m-1; j >= 0; j--)
+            int midx = mid/m;
+            int midy = mid%m;
+
+            if(matrix[midx][midy] >= target)
             {
-                if(matrix[i][j] == target) return true;
+                r = mid;
+            }
+            else
+            {
+                l = mid + 1;
             }
         }
+
+        if(l < n*m && matrix[l/m][l%m] == target) return true;
 
         return false;
     }
