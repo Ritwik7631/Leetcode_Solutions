b8a555859f24f92ad34bf34357da9008f78af3de
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-05 18:13:12 -0700

    Time: 4 ms (22.88%) | Memory: 8.2 MB (72.91%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        }
+        if(n == 0) return 1;
 
-        double half = myPow(x, n / 2);
+        if(n == 1) return x;
 
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            if (n > 0) {
-                return half * half * x;
-            } else {
-                return half * half / x;
+        if(x == 0) return 0;
+
+        bool flip = n < 0;
+
+        int rem = abs(n);
+        double ans = 1;
+        while(rem > 0)
+        {
+            if(rem % 2 == 1)
+            {
+                ans *= x;
+                rem--;
+            }
+            else
+            {
+                x *= x;
+                rem /= 2;
             }
         }
+
+        if(flip) return 1/ans;
+
+        return ans;
     }
 
 };
