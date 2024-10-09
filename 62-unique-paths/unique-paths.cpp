bab85b1cea3cb2683edbefc0c103f0e992c779a6
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-09 13:23:07 -0700

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.11%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fn(int i, int j, vector<vector<int>> &dp)
+    {
+        if(i == 0 && j == 0) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int left = 0, up = 0;
+
+        if(j - 1 >= 0) left = fn(i, j-1, dp);
+
+        if(i - 1 >= 0) up = fn(i-1, j, dp);
+
+        return dp[i][j] = up + left; 
+    }
+
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        return fn(n-1, m-1, dp);
+    }
+};
