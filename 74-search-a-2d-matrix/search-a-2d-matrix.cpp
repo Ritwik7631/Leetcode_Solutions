7fc5e6d739e2edb572e3a2e7a2eb7ceaae8bf8e1
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-09 19:14:28 -0700

    Time: 0 ms (100.00%) | Memory: 12.1 MB (92.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -18,32 +18,60 @@ public:
 
         // return false;
 
+        // not efficient approach
+
 // --------------------------------------------------------------------------------
         int n = matrix.size();
         int m = matrix[0].size();
 
+        // binary search on row to find a row element less than or equal to the target
+
         int l = 0;
-        int r = n*m;
+
+        int r = n-1;
 
         while(l < r)
         {
-            int mid = (r-l)/2 + l;
+            int mid = (r-l+1)/2 + l;
 
-            int midx = mid/m;
-            int midy = mid%m;
-
-            if(matrix[midx][midy] >= target)
+            if(matrix[mid][0] <= target)
             {
-                r = mid;
+                l = mid;
             }
             else
             {
-                l = mid + 1;
+                r = mid - 1;
             }
         }
 
-        if(l < n*m && matrix[l/m][l%m] == target) return true;
+        // now l stores the index of the row that must contain the answer
+
+        int correct_row = l;
+
+        l = 0;
+
+        r = m-1;
+
+        while(l <= r)
+        {
+            int mid = (r-l)/2 + l;
+
+            if(matrix[correct_row][mid] == target)
+            {
+                return true;
+            }
+            else if(matrix[correct_row][mid] < target)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
 
         return false;
+
+
     }
 };
