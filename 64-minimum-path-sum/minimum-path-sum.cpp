9d03c6f398c45de9bdcf423272570d23e5051eeb
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-12 19:36:31 -0700

    Time: 7 ms (87.63%) | Memory: 14.7 MB (55.01%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -24,38 +24,73 @@ public:
         // return fn(n-1, m-1, grid, dp);
 
 // -----------------------------------------------------------------
+        // int n = grid.size();
+        // int m = grid[0].size();
+        // vector<vector<int>> dp(n, vector<int> (m, 0));
+
+        // dp[0][0] = grid[0][0];
+
+        // for(int j = 1; j < m; j++)
+        // {
+        //     dp[0][j] += (grid[0][j] + dp[0][j-1]);
+        // }
+
+        // for(int i = 1; i < n; i++)
+        // {
+        //     dp[i][0] += (grid[i][0] + dp[i-1][0]);
+        // }
+
+        // for(int i = 1; i < n; i++)
+        // {
+        //     for(int j = 1; j < m; j++)
+        //     {
+        //         int up = INT_MAX;
+        //         int left = INT_MAX;
+
+        //         if(i-1 >= 0) up = grid[i][j] + dp[i-1][j];
+
+        //         if(j-1 >= 0) left = grid[i][j] + dp[i][j-1];
+
+        //         dp[i][j] = min(up, left);
+        //     }
+        // }
+
+        // return dp[n-1][m-1];
+
+// -------------------------------------------------------------------
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int> (m, 0));
 
-        dp[0][0] = grid[0][0];
+        vector<int> prev(m, 0);
+        
+
+        prev[0] = grid[0][0];
 
         for(int j = 1; j < m; j++)
         {
-            dp[0][j] += (grid[0][j] + dp[0][j-1]);
+            prev[j] += (prev[j-1] + grid[0][j]);
         }
 
         for(int i = 1; i < n; i++)
         {
-            dp[i][0] += (grid[i][0] + dp[i-1][0]);
-        }
-
-        for(int i = 1; i < n; i++)
-        {
+            vector<int> curr(m, 0);
+            curr[0] = (grid[i][0] + prev[0]);
             for(int j = 1; j < m; j++)
             {
                 int up = INT_MAX;
                 int left = INT_MAX;
 
-                if(i-1 >= 0) up = grid[i][j] + dp[i-1][j];
+                if(i-1 >= 0) up = grid[i][j] + prev[j];
 
-                if(j-1 >= 0) left = grid[i][j] + dp[i][j-1];
+                if(j-1 >= 0) left = grid[i][j] + curr[j-1];
 
-                dp[i][j] = min(up, left);
+                curr[j] = min(up, left);
             }
+
+            prev = curr;
         }
 
-        return dp[n-1][m-1];
+        return prev[m-1];
 
     }
 };
