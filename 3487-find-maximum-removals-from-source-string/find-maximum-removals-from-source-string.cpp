85fc045bec04d458341b90f4e0466ee46a5978de
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-14 09:12:17 -0700

    Time: 197 ms (31.82%) | Memory: 92.9 MB (9.09%) - LeetSync

--- 3487-find-maximum-removals-from-source-string/find-maximum-removals-from-source-string.cpp
+++ 3487-find-maximum-removals-from-source-string/find-maximum-removals-from-source-string.cpp
@@ -1,207 +1,55 @@
 class Solution {
 public:
-    int fn(int sindex, int pindex, int s, int p, const vector<bool> &f, 
-           const string &source, const string &pattern, vector<vector<int>> &memo)
+    int fn(int i, int j, int k, string &source, string &pattern, vector<int> &ti, vector<vector<int>> &dp)
     {
-        // Base Case 1: Reached end of source
-        if(sindex == s)
-        {
-            if(pindex == p)
-                return 0;
-            else
-                return INT_MIN;
-        }
+        if(j < 0) return k + 1;
+    
+        if(i < 0) return INT_MIN;
 
-        // Base Case 2: Entire pattern has been matched
-        if(pindex == p)
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int match = INT_MIN;
+        int notmatch = INT_MIN;
+        
+        if(source[i] == pattern[j])
         {
-            if(f[sindex])
+            if(k >= 0 && i == ti[k])
             {
-                int res = fn(sindex + 1, pindex, s, p, f, source, pattern, memo);
-                if(res != INT_MIN)
-                    return 1 + res;
-                else
-                    return INT_MIN;
+                notmatch = fn(i-1, j, k-1, source, pattern, ti, dp) + 1;
+                match = fn(i-1, j-1, k-1, source, pattern, ti, dp);
+                return dp[i][j] = max(notmatch, match);
             }
             else
             {
-                return fn(sindex + 1, pindex, s, p, f, source, pattern, memo);
+                match = fn(i-1, j-1, k, source, pattern, ti, dp);
+                notmatch = fn(i-1, j, k, source, pattern, ti, dp);
+                return dp[i][j] = max(match, notmatch);
             }
         }
-
-        // Check if result is already computed
-        if(memo[sindex][pindex] != -1)
-            return memo[sindex][pindex];
-
-        int best = INT_MIN;
-
-        // Option 1: Take the current character for the pattern if it matches
-        if(source[sindex] == pattern[pindex])
+        else
         {
-            int res = fn(sindex + 1, pindex + 1, s, p, f, source, pattern, memo);
-            if(res != INT_MIN)
-                best = max(best, res);
+            if(k >= 0 && i == ti[k])
+            {
+                notmatch = fn(i-1, j, k-1, source, pattern, ti,dp) + 1;
+                return dp[i][j] = max(notmatch, match);
+            }
+            else
+            {
+                notmatch = fn(i-1, j, k, source, pattern, ti,dp);
+                return dp[i][j] = max(notmatch, match);
+            }
         }
-
-        // Option 2: Remove the current character if it's removable
-        if(f[sindex])
-        {
-            int res = fn(sindex + 1, pindex, s, p, f, source, pattern, memo);
-            if(res != INT_MIN)
-                best = max(best, 1 + res);
-        }
-
-        // Option 3: Skip the current character without removing
-        int res = fn(sindex + 1, pindex, s, p, f, source, pattern, memo);
-        if(res != INT_MIN)
-            best = max(best, res);
-
-        // Memoize the result
-        memo[sindex][pindex] = best;
-
-        return best;
+        
+        return INT_MIN;
     }
 
     int maxRemovals(string source, string pattern, vector<int>& targetIndices) {
-        int s = source.size();
-        int p = pattern.size();
+        int n = source.size();
+        int m = pattern.size();
+        int k = targetIndices.size();
 
-        // Convert targetIndices to a vector<bool> for O(1) lookups
-        vector<bool> f(s, false);
-        for(auto index : targetIndices){
-            if(index >= 0 && index < s){
-                f[index] = true;
-            }
-        }
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
 
-        // Initialize memoization table with -1
-        // Dimensions: s x p
-        // Note: If p is 0, no characters need to be matched
-        // If s is 0, and p is not 0, it's impossible
-        // Initialize with -1 indicating uncomputed states
-        vector<vector<int>> memo(s, vector<int>(p, -1));
-
-        // Start recursion from sindex = 0 and pindex = 0
-        int result = fn(0, 0, s, p, f, source, pattern, memo);
-
-        // If result is negative, it means it's impossible to match the pattern
-        if(result == INT_MIN)
-            return 0;
-        else
-            return result;
+        return fn(n-1,m-1, k-1, source, pattern, targetIndices, dp);
     }
 };
-
-// class Solution {
-// public:
-//     bool fx(int i, string &source, string &pattern, vector<int>& ti)
-//     {
-//         char original = source[ti[i]];
-//         source[ti[i]] = 'X';
-
-//         int n = source.size();
-//         int m = pattern.size();
-
-//         int x = 0;
-//         int y = 0;
-
-//         while(x < n && y < m)
-//         {
-//             if(source[x] == pattern[y])
-//             {
-//                 y++;
-//             }
-
-//             x++;
-//         }
-
-//         // cout << source << " " << pattern << endl;
-
-//         // if(y == m) cout << "pattern match" << endl;
-//         // else cout << "no match" << endl;
-
-//         source[ti[i]] = original;
-        
-//         return y == m;
-//     }
-
-//     int fn(int i, string &source, string &pattern, vector<int>& ti, vector<int> &dp)
-//     {
-//         if(i < 0) return 0;
-
-//         if(dp[i] != -1) return dp[i];
-//         int pick = INT_MIN;
-//         int notpick = INT_MIN;
-
-//         notpick = fn(i-1, source, pattern, ti, dp);
-
-//         if(fx(i, source, pattern, ti) == true)
-//         {
-//             char original = source[ti[i]];
-//             source[ti[i]] = 'X';
-//             pick = 1 + fn(i-1, source, pattern, ti, dp);
-//             source[ti[i]] = original;
-//         }
-
-//         return dp[i] = max(pick, notpick);
-//     }
-
-//     int maxRemovals(string source, string pattern, vector<int>& targetIndices) {
-//         int n = targetIndices.size();
-
-//         vector<int> dp(n+1, -1);
-
-//         return fn(n-1, source, pattern, targetIndices, dp);
-//     }
-// };
-
-
-
-
-// --------------------------------------------------------------------------------
-// class Solution {
-// public:
-//     int fn(int i, string source, string pattern, vector<int>& targetIndices)
-//     {
-//         bool flag = false;
-
-//         char original = source[i];
-
-//         source[i] = 'Z';
-
-//         flag = fx(pattern, source);
-
-//         int ans = 0;
-
-//         if(flag)
-//         {
-//             targetIndices[i] = -1;
-//             for(int j = 0; j < targetIndices.size(); j++)
-//             {
-//                 int operations = 0;
-//                 if(targetIndices[j] != -1)
-//                 {
-//                     operations += 1;
-//                     operations += fn(j, source, pattern, targetIndices);
-//                 }
-//                 ans = max(ans, operations);
-//             }
-//         }
-//         else
-//         {
-               
-//         }
-
-//         source[i] = original;
-//     }
-
-//     int maxRemovals(string source, string pattern, vector<int>& targetIndices) {
-//        int count = INT_MIN;
-
-//        for(int i = 0; i < targetIndices.size(); i++)
-//        {
-//             vector<int> temp = targetIndices;
-//             count = max(count, fn(targetIndices[i], source, pattern, temp));
-//        } 
-//     }
-// };
