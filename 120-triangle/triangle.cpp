b1feb92e076b1423d6a22d48a62c155e862ae14f
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-10-16 12:54:23 -0700

    Time: 4 ms (74.76%) | Memory: 11.8 MB (34.53%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int fn(int i, int j, vector<vector<int>>& triangle, vector<vector<int>> &dp)
     {
-        int n = triangle.size();
-        if(i == triangle.size()-1) return triangle[i][j];
+        if(i >= triangle.size()) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int left = INT_MAX;
+        int choice2 = INT_MAX;
 
-        int right = INT_MAX;
+        int choice1 = triangle[i][j] + fn(i+1, j, triangle,dp);
 
-        if(i + 1 < n) left = fn(i+1, j, triangle, dp);
+        if(i+1 < triangle.size() && j+1 < triangle[i+1].size()) choice2 = triangle[i][j] + fn(i+1, j+1, triangle,dp);
 
-        if(i + 1 < n && j + 1 < triangle[i+1].size()) right = fn(i+1, j+1, triangle, dp);
-
-        return dp[i][j] = min(left, right) + triangle[i][j];
+        return dp[i][j] = min(choice1, choice2);
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return fn(0, 0, triangle,dp);
+        // fn(i,j) the minimum path from the top the of triangle to i and j
+        vector<vector<int>> dp(triangle.size(), vector<int> (triangle.size(), -1));
+        return fn(0,0, triangle,dp);
     }
 };
