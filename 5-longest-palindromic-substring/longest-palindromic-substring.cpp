208c932f664fa4ed7fa695d10c593055d513e748
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-12-01 17:01:41 -0800

    Time: 2 ms (98.21%) | Memory: 11.3 MB (65.47%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,132 +1,69 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string modified = "^";
+        string modified = "^#";
 
         for(auto a : s)
         {
             modified += a;
             modified += '#';
         }
-        
-        modified[modified.size()-1] = '$';
 
-        vector<int> radius_centered_at_i(modified.size(), 0);
-        
+        modified += '$';
+
         int l = 1;
         int r = 1;
         int n = modified.size();
-        for(int i = 1; i < n; i++)
+
+        vector<int> radius(n, 0);
+
+        for(int i = 1; i < n-1; i++)
         {
-            // cout << "l: " << l << " r: " << r << " i:" << i << endl;
-            // int mirrored_position = l + r - i;
+            int mirrored_position = l + r - i;
 
-            // cout << mirrored_position << endl;
+            int dist_to_edge_from_i = r - i;
 
-            radius_centered_at_i[i] = max(0, min(r-i, radius_centered_at_i[l+r-i]));
-            
-            while(i + radius_centered_at_i[i] + 1 < n &&
-                  i - radius_centered_at_i[i] - 1 >= 0 &&
-                  modified[i + radius_centered_at_i[i] + 1] == modified[i - radius_centered_at_i[i] - 1])
-                  {
-                        radius_centered_at_i[i]++;
-                  }
-
-            int current_l = i - radius_centered_at_i[i];
-            int current_r = i + radius_centered_at_i[i];
-
-            if(current_r > r)
+            if(dist_to_edge_from_i > 0)
             {
-                r = current_r;
-                l = current_l;
-            }
-
-            // for(auto a : radius_centered_at_i)
-            // {
-            //     cout << a << "  ";
-            // }
-            // cout << endl;
-
-            // for(auto a : modified)
-            // {
-            //     cout << a << "  ";
-            // }
-            // cout << endl;
-
-            // for(int i = 0; i < modified.size(); i++)
-            // {
-            //     if(i < 10) cout << 0;
-            //     cout << i << " ";
-            // }
-            // cout << endl;
-            // cout << "------------------------------" << endl;            
-        }
-
-        // radius_centered_at_i[0] = -1;
-        // radius_centered_at_i[modified.size()-1] = -1;
-
-        int max_length = 0;
-        int start, end;
-        int c;
-
-        for(int i = 1; i < radius_centered_at_i.size() - 1; i++)
-        {
-            int r = radius_centered_at_i[i];
-
-            if(modified[i] == '#')
-            {
-                if(r%2 == 0) r--;
+                if(radius[mirrored_position] < dist_to_edge_from_i)
+                {
+                    radius[i] = radius[mirrored_position];
+                }
+                else
+                {
+                    radius[i] = dist_to_edge_from_i;
+                }
             }
             else
             {
-                if(r%2 == 1) r--;
+                radius[i] = 0;
             }
 
-            int current_length = r + 1;
-
-            if(current_length >= max_length)
+            while(i + radius[i] + 1 < n && i - radius[i] - 1 && modified[i + radius[i] + 1] == modified[i - radius[i] - 1])
             {
-                max_length = current_length;
-                c = i;
+                radius[i]++;
             }
+
+            if(i + radius[i] > r)
+            {
+                l = i - radius[i];
+                r = i + radius[i];
+            } 
         }
-
-        // for(auto a : radius_centered_at_i)
-        // {
-        //     cout << a << "  ";
-        // }
-        // cout << endl;
-
-        start = c - radius_centered_at_i[c];
-        end = c + radius_centered_at_i[c];
-
-        // for(auto a : modified)
-        // {
-        //     cout << a << "  ";
-        // }
-        // cout << endl;
-
-        // for(int i = 0; i < modified.size(); i++)
-        // {
-        //     if(i < 10) cout << 0;
-        //     cout << i << " ";
-        // }
-        // cout << endl;
-
-        // cout << "s: " << start << endl;
-        // cout << "e: " << end << endl;
-        // cout << "c: " << c << endl;
 
         string ans = "";
 
-        for(int x = start; x <= end; x++)
-        {
-            if(modified[x] != '#')
-            {
-                ans += modified[x];
+        int max_length = 0;
+        int center_index = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (radius[i] > max_length) {
+                max_length = radius[i];
+                center_index = i;
             }
         }
 
-        return ans;
+        int start = (center_index - max_length) / 2;
+        return s.substr(start, max_length);
+
     }
 };
