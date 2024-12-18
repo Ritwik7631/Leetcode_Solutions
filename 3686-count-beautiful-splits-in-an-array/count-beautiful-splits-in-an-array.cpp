91324d252aa1b8b369c15fe19cca514cc73ab097
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-12-18 13:03:19 -0800

    Time: 475 ms (54.98%) | Memory: 27 MB (70.88%) - LeetSync

--- 3686-count-beautiful-splits-in-an-array/count-beautiful-splits-in-an-array.cpp
+++ 3686-count-beautiful-splits-in-an-array/count-beautiful-splits-in-an-array.cpp
@@ -1,63 +1,54 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    const int mod = 1e9 + 7;
-    const int base = 51; // Base should be greater than the maximum possible nums[i]
+    int mod = 1e9+7;
+    vector<long long> prefixHash; // hash[i] return hash from 0 to i
+    vector<long long> powerBase; // powers of the (base^i)%mod
 
-    vector<long long> prefixHash;
-    vector<long long> powerBase;
-
-    // Function to precompute prefix hashes and power bases
-    void computeHash(const vector<int>& nums){
+    void computeHashes(vector<int>& nums) {
         int n = nums.size();
         prefixHash.resize(n + 1, 0);
         powerBase.resize(n + 1, 1);
-        for(int i = 0; i < n; i++){
-            prefixHash[i + 1] = (prefixHash[i] * base + nums[i]) % mod;
-            powerBase[i + 1] = (powerBase[i] * base) % mod;
+
+        for (int i = 1; i <= n; i++) {
+            powerBase[i] = (powerBase[i-1] * 51) % mod;
+        }
+
+        for (int i = 0; i < n; i++) {
+            prefixHash[i+1] = (prefixHash[i] * 51 + nums[i]) % mod;
         }
     }
 
-    // Function to get the hash of subarray [start, start + len)
-    long long getHash(int start, int len){
-        return (prefixHash[start + len] - (prefixHash[start] * powerBase[len]) % mod + mod) % mod;
+    int hash(int start, int len) {
+        return (prefixHash[start+len] - (prefixHash[start] * powerBase[len]) % mod + mod) % mod;
     }
 
     int beautifulSplits(vector<int>& nums) {
         int n = nums.size();
-        computeHash(nums);
+        computeHashes(nums);
         int count = 0;
 
-        // Iterate through all possible split points
-        for(int i = 1; i <= n - 2; i++) { // Split after index i-1 for nums1
-            for(int j = i + 1; j <= n - 1; j++) { // Split after index j-1 for nums2
+        for (int i = 1; i <= n-2; i++) {   
+            for (int j = i+1; j <= n-1; j++) {
                 bool cond1 = false, cond2 = false;
 
                 // Condition 1: nums1 is a prefix of nums2
-                if(j - i >= i){
-                    long long h1 = getHash(0, i);      // Hash of nums1
-                    long long h2 = getHash(i, i);      // Hash of the first i elements of nums2
-                    if(h1 == h2){
-                        cond1 = true;
-                    }
+                if (j - i >= i - 0) {
+                    int h1 = hash(0, i); // nums1
+                    int h2 = hash(i, i); // nums2 of length nums1
+                    if (h1 == h2) cond1 = true;
                 }
 
                 // Condition 2: nums2 is a prefix of nums3
-                if(n - j >= j - i){
-                    long long h3 = getHash(i, j - i);  // Hash of nums2
-                    long long h4 = getHash(j, j - i);  // Hash of the first (j-i) elements of nums3
-                    if(h3 == h4){
-                        cond2 = true;
-                    }
+                if (n - j >= j - i) {
+                    int h3 = hash(i, j - i); // nums2
+                    int h4 = hash(j, j - i); // nums3 of length nums2
+                    if (h3 == h4) cond2 = true;
                 }
 
-                if(cond1 || cond2){
-                    count++;
-                }
+                if (cond1 || cond2) count++;
             }
         }
+
         return count;
     }
 };
