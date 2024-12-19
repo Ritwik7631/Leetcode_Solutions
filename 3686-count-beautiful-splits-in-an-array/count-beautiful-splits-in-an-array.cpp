885d6c9001c23549c58d6d3ab75c86d941f87d22
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2024-12-18 17:48:31 -0800

    Time: 478 ms (54.52%) | Memory: 27 MB (64.71%) - LeetSync

--- 3686-count-beautiful-splits-in-an-array/count-beautiful-splits-in-an-array.cpp
+++ 3686-count-beautiful-splits-in-an-array/count-beautiful-splits-in-an-array.cpp
@@ -1,51 +1,72 @@
 class Solution {
 public:
+    vector<long long> prefixhash; // prefixhash[i] represents the hash from 0 to i
+    vector<long long> basepower; // basepower[i] represents the 
     int mod = 1e9+7;
-    vector<long long> prefixHash; // hash[i] return hash from 0 to i
-    vector<long long> powerBase; // powers of the (base^i)%mod
-
-    void computeHashes(vector<int>& nums) {
+    void fn(vector<int>& nums)
+    {
         int n = nums.size();
-        prefixHash.resize(n + 1, 0);
-        powerBase.resize(n + 1, 1);
 
-        for (int i = 1; i <= n; i++) {
-            powerBase[i] = (powerBase[i-1] * 51) % mod;
+        prefixhash.resize(n+1, 0);
+        basepower.resize(n+1, 1);
+        int base = 57;
+        // we populate basepower in n time
+        for(int i = 1; i <= n; i++)
+        {
+            basepower[i] = (basepower[i-1] * base) % mod;
         }
-
-        for (int i = 0; i < n; i++) {
-            prefixHash[i+1] = (prefixHash[i] * 51 + nums[i]) % mod;
+        // we populate prefixhash in n time
+        for(int i = 0; i < n; i++)
+        {
+            prefixhash[i+1] = (prefixhash[i] + (nums[i]*basepower[i])%mod)%mod;
         }
+        
     }
 
-    int hash(int start, int len) {
-        return (prefixHash[start+len] - (prefixHash[start] * powerBase[len]) % mod + mod) % mod;
+    int hash(int start, int len)
+    {
+        return (prefixhash[start+len] - (prefixhash[start]*basepower[len])%mod + mod)%mod;
     }
 
     int beautifulSplits(vector<int>& nums) {
         int n = nums.size();
-        computeHashes(nums);
+        fn(nums);
+
+        // for(auto a : prefixhash)
+        // {
+        //     cout << a << " ";
+        // }
+        // cout << endl;
+
         int count = 0;
 
-        for (int i = 1; i <= n-2; i++) {   
-            for (int j = i+1; j <= n-1; j++) {
+        for(int i = 1; i <= n-2; i++)
+        {
+            for(int j = i+1; j <= n-1; j++)
+            {
                 bool cond1 = false, cond2 = false;
 
-                // Condition 1: nums1 is a prefix of nums2
-                if (j - i >= i - 0) {
-                    int h1 = hash(0, i); // nums1
-                    int h2 = hash(i, i); // nums2 of length nums1
-                    if (h1 == h2) cond1 = true;
+                if(j-i >= i - 0)
+                {
+                    int h1 = hash(0, i);
+                    int h2 = hash(i, i);
+
+                    // cout << "h1: " << h1 << " " << " h2: " << h2 << endl;
+
+                    if(h1 == h2) cond1 = true;
                 }
 
-                // Condition 2: nums2 is a prefix of nums3
-                if (n - j >= j - i) {
-                    int h3 = hash(i, j - i); // nums2
-                    int h4 = hash(j, j - i); // nums3 of length nums2
-                    if (h3 == h4) cond2 = true;
+                if(n-j >= j-i)
+                {
+                    int h3 = hash(i, j-i);
+                    int h4 = hash(j, j-i);
+
+                    // cout << "h3: " << h3 << " " << " h4: " << h4 << endl;
+
+                    if(h3 == h4) cond2 = true;
                 }
 
-                if (cond1 || cond2) count++;
+                if(cond1 || cond2) count++;
             }
         }
 
