e792684ac525d44c67e207d954fa4537366fa0fc
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-02-17 16:38:04 -0800

    Time: 47 ms (24.89%) | Memory: 75.6 MB (17.21%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+
+        for(auto a : nums)
+        {
+            mp[a]++;
+        }
+
+        for(auto a : mp)
+        {
+            if(a.second >= 2) return true;
+        }
+
+        return false;
+    }
+};
