e277980060be08940dc67d682050458ff354bd0e
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-02-18 18:21:49 -0800

    Time: 0 ms (100.00%) | Memory: 40.2 MB (79.60%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,54 +2,29 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefix(n,0);
-        vector<int> suffix(n,0);
 
-        for(int i = 0; i < n; i++)
+        vector<int> ans(n, 1);
+
+        for(int i = 1; i < n; i++)
         {
-            if(i == 0)
-            {
-                prefix[i] = nums[i];
-            }
-            else
-            {
-                prefix[i] = prefix[i-1] * nums[i];
-            }
+            ans[i] = ans[i-1] * nums[i-1];
         }
 
-        vector<int> dupl = nums;
-        reverse(dupl.begin(), dupl.end());
+        // 1, 1, 2, 6
 
-        for(int i = 0; i < n; i++)
+        // 24,12,4,1  
+
+        int pre = 1;
+
+        for(int i = n-1; i >= 0; i--)
         {
-            if(i == 0)
-            {
-                suffix[i] = dupl[i];
-            }
-            else
-            {
-                suffix[i] = suffix[i-1] * dupl[i];
-            }
-        }
 
-        for(auto a : prefix) cout << a << " ";
-        cout << endl;
+            ans[i] = ans[i] * pre;
 
-        for(auto a : suffix) cout << a << " ";
-        cout << endl;
-
-        vector<int> ans(n, 0);
-
-        for(int i = 0; i < n; i++)
-        {
-            int pre = 1, suf = 1;
-
-            if(i-1 >= 0) pre = prefix[i-1];
-            if(n - 1 - i - 1 >= 0) suf = suffix[n-2-i];
-
-            ans[i] = suf*pre;
+            pre *= nums[i];
         }
 
         return ans;
+
     }
 };
