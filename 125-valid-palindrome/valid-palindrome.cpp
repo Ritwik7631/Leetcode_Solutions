ea80b8f55d9cb12d3d9e143bd4a0ed0bc5e656e1
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-02-19 22:21:04 -0800

    Time: 0 ms (100.00%) | Memory: 10.9 MB (6.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    bool valid(char x)
+    {
+        x = tolower(x);
+
+        if(int(x) >= int('a') && int(x) <= 'z' || int(x) >= int('0') && int(x) <= '9') return true;
+
+        return false;
+    }
+
     bool isPalindrome(string s) {
         string org = "";
 
         for(auto a : s)
         {
-            if(isalnum(a))
+            if(valid(a))
             {
                 org += (char)tolower(a);
             }
