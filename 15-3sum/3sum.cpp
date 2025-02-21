97e729b87495eb5d7ec6c8226bb02517103eb272
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-02-20 20:01:04 -0800

    Time: 49 ms (61.45%) | Memory: 29.1 MB (66.99%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+
         sort(nums.begin(), nums.end());
 
         vector<vector<int>> ans;
 
-        for(int i = 0; i < nums.size(); i++)
+        for(int a = 0; a < n; a++)
         {
-            if(i-1 >= 0 && nums[i] == nums[i-1]) continue;
+            if(a > 0 && nums[a] == nums[a-1]) continue;
 
-            // cout << "i: " << i << endl;
-            // cout << "nums[i]: " << nums[i] << endl;
+            int b = a + 1;
+            int c = n - 1;
 
-            int j = i + 1;
-            int k = nums.size()-1;
-
-            while(j < k)
+            while(b < c)
             {
-                // cout << "j: " << j << " " << "k: " << k << endl;
-                // cout << "nums[j]: " << nums[j] << " " << "nums[k]: " << nums[k] << endl;
-                int sum = nums[i] + nums[j] + nums[k];
-                // cout << "sum: " << sum << endl;
-                if(sum == 0)
+                int x = nums[a] + nums[b] + nums[c];
+                if(x == 0)
                 {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
+                    ans.push_back({nums[a], nums[b], nums[c]});
 
-                    while(j < nums.size() && j-1 >= i+1 && nums[j] == nums[j-1]) j++;
-                    while(k >= i+1 && k+1 < nums.size() && nums[k] == nums[k+1]) k--;
+                    while(b < c && nums[b] == nums[b+1]) b++;
+
+                    while(b < c && nums[c] == nums[c-1]) c--;
+
+                    b++;
+                    c--;
                 }
-                else if(sum > 0)
+                else if(x < 0)
                 {
-                    k--;
+                    b++;
                 }
                 else
                 {
-                    j++;
+                    c--;
                 }
             }
         }
