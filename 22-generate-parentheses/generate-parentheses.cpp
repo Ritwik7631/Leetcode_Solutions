0a86a921e502f0f9bf91b082689593643e74acc8
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-02-22 18:13:53 -0800

    Time: 3 ms (70.61%) | Memory: 15.5 MB (63.36%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    vector<string> ans;
-    bool valid(string &temp)
-    {
-        stack<char> stk;
-
-        for(auto a : temp)
-        {
-            if(a == '(') stk.push('(');
-            else
-            {
-                if(stk.empty()) return false;
-                stk.pop();
-            }
-        }
-
-        return stk.empty();
-    }
-
-    void fn(int o, int c, string temp)
+    void fn(int o, int c, string temp, vector<string> &ans)
     {
         if(o <= 0 && c <= 0)
         {
-            if(valid(temp)) ans.push_back(temp);
+            ans.push_back(temp);
 
             return;
         }
 
         if(o - 1 >= 0)
         {
-            fn(o-1, c, temp + "(");
+            fn(o-1, c, temp + "(", ans);
         }
 
-        if(c - 1 >= 0)
+        if(c > o)
         {
-            fn(o, c-1, temp + ")");
+            fn(o, c-1, temp + ")", ans);
         }
     }
 
     vector<string> generateParenthesis(int n) {
-        fn(n,n, "");
+        vector<string> ans;
+        fn(n,n, "", ans);
 
         return ans;
     }
