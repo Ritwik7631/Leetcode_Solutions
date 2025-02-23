6ebc6f4cb0e31d044c9bac20b2214ca655bafe8e
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-02-23 11:42:36 -0800

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.66%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,77 +1,32 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // int n = matrix.size();
-        // int m = matrix[0].size();
-
-        // for(int i = n-1; i >= 0; i--)
-        // {   
-        //     if(matrix[i][m-1] < target) continue;
-
-        //     if(matrix[i][m-1] == target) return true;
-
-        //     for(int j = m-1; j >= 0; j--)
-        //     {
-        //         if(matrix[i][j] == target) return true;
-        //     }
-        // }
-
-        // return false;
-
-        // not efficient approach
-
-// --------------------------------------------------------------------------------
         int n = matrix.size();
+
         int m = matrix[0].size();
 
-        // binary search on row to find a row element less than or equal to the target
-
         int l = 0;
-
-        int r = n-1;
+        int r = n*m - 1;
 
         while(l < r)
         {
-            int mid = (r-l+1)/2 + l;
+            int mid = l + (r-l)/2;
 
-            if(matrix[mid][0] <= target)
-            {
-                l = mid;
-            }
-            else
-            {
-                r = mid - 1;
-            }
-        }
+            pair<int,int> mid_coordinates;
 
-        // now l stores the index of the row that must contain the answer
+            mid_coordinates.first = mid / m;
+            mid_coordinates.second = mid % m;
 
-        int correct_row = l;
-
-        l = 0;
-
-        r = m-1;
-
-        while(l <= r)
-        {
-            int mid = (r-l)/2 + l;
-
-            if(matrix[correct_row][mid] == target)
-            {
-                return true;
-            }
-            else if(matrix[correct_row][mid] < target)
+            if(matrix[mid_coordinates.first][mid_coordinates.second] < target)
             {
                 l = mid + 1;
             }
             else
             {
-                r = mid - 1;
+                r = mid;
             }
         }
 
-        return false;
-
-
+        return matrix[l/m][l%m] == target;
     }
 };
