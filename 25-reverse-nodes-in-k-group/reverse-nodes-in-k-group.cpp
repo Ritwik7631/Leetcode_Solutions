40fa8fa50733552aad040c0cce031801f9a06bb4
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-02-25 12:44:16 -0800

    Time: 0 ms (100.00%) | Memory: 16.4 MB (95.92%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -15,54 +15,72 @@ public:
         if(a->next == b)
         {
             b->next = a;
-            a->next = nullptr;
             return;
         }
+
         rv(a->next, b);
+
         a->next->next = a;
     }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head || k <= 1) return head;
-        
+        if(k == 1) return head;
+
         ListNode* anshead = nullptr;
-        ListNode* prevTail = nullptr;  // To connect previous reversed group
-        
-        ListNode* mover = head;
-        int c = -1;
+
+        ListNode* currend;
+
+        int s = 0;
+        int e = k-1;
+
         ListNode* start = nullptr;
         ListNode* end = nullptr;
-        int s = 0;
-        int e = k - 1;
-        
+
+        ListNode* mover = head;
+
+        int c = -1;
+
         while(mover != nullptr)
         {
             c++;
-            if(c % k == s)
+
+            if(c == s)
+            {
                 start = mover;
-            if(c % k == e)
+            }
+            
+            if(c == e)
             {
                 end = mover;
+
                 ListNode* nxt = mover->next;
-                
-                rv(start, end);      // Reverse current group
-                start->next = nxt;   // Connect reversed group tail to next part
-                
-                if(prevTail != nullptr)
-                    prevTail->next = end;  // Connect previous group to current group
+
+                rv(start, end);
+
+                start->next = nxt;
+
+                if(anshead == nullptr)
+                {
+                    anshead = end;
+                    currend = start;
+                }
                 else
-                    anshead = end;         // First reversed group becomes the new head
-                
-                prevTail = start;          // Update previous tail for next reversal
-                
-                // Jump mover ahead to the next group's start and reset counter
+                {
+                    currend->next = end;
+                    currend = start;
+                }
+
+                c = -1;
+
                 mover = nxt;
-                c = -1;  // reset counter for the new group
-                continue;  // skip the mover = mover->next below
+                continue;
             }
-            mover = mover->next; // Advance pointer normally if group not complete
+
+            mover = mover->next;
         }
-        
-        return anshead ? anshead : head;
+
+        if(anshead == nullptr) return head;
+
+        return anshead;
     }
 };
