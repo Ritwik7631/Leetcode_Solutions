86040a15a7830972de81c146de1b2d0e78514c00
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-03 23:54:01 -0800

    Time: 58 ms (45.95%) | Memory: 79.9 MB (28.22%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-
-    bool isPalindrome(string x)
-    {
-        int n = x.size();
-        for(int i = 0; i < n/2; i++)
-        {
-            if(x[i] != x[n-1-i]) return false;
+    
+    // Helper function that checks if s[i...j] is a palindrome.
+    bool isPalindrome(const string &s, int i, int j) {
+        while(i < j) {
+            if(s[i] != s[j])
+                return false;
+            i++;
+            j--;
         }
-
         return true;
     }
-
-    void fn(int i, string s, vector<string>& curr)
-    {
-        if(i == s.size())
-        {
-            ans.push_back(curr);
+    
+    // Backtracking function that uses start index.
+    void backtrack(int start, const string &s, vector<string>& current) {
+        if(start == s.size()) {
+            ans.push_back(current);
             return;
         }
-
-        string temp = "";
-
-        for(int j = i; j < s.size(); j++)
-        {
-            temp += s[j];
-
-            if(isPalindrome(temp))
-            {
-                curr.push_back(temp);
-                fn(j+1, s, curr);
-                curr.pop_back();
+        
+        // Try all possible end indices for the substring starting at 'start'.
+        for(int end = start; end < s.size(); end++){
+            // If the current substring s[start...end] is a palindrome,
+            // then add it to the current partition and proceed recursively.
+            if(isPalindrome(s, start, end)) {
+                current.push_back(s.substr(start, end - start + 1));
+                backtrack(end + 1, s, current);
+                current.pop_back();
             }
         }
     }
-
+    
     vector<vector<string>> partition(string s) {
-        
-        vector<string> curr;
-
-        fn(0, s, curr);
-
+        vector<string> current;
+        backtrack(0, s, current);
         return ans;
     }
 };
