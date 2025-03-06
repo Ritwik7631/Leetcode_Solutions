5eb6b229cc1b24265b4e7de27d09ddf160ea0e4c
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-06 00:06:40 -0800

    Time: 59 ms (16.19%) | Memory: 49.7 MB (22.64%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,71 +1,72 @@
 class Solution {
 public:
-    const long long mod = 1e9 + 7;
-    // int fn(int i, int a, vector<int>& coins, vector<vector<int>> &dp)
-    // {   
-        // if(a == 0) return 0;
+    int fn(int i, int a, vector<int>& coins, vector<vector<int>> &dp)
+    {   
+        if(a == 0) return 0;
 
-        // if(i < 0) return 1000000;
+        if(i < 0) return INT_MAX;
 
-        // if(dp[i][a] != -1) return dp[i][a];
+        if(dp[i][a] != -1) return dp[i][a];
 
-        // int notpick = fn(i-1, a, coins, dp);
+        int notpick = fn(i-1, a, coins, dp);
 
-        // int pick = 1000000;
+        int pick = INT_MAX;
 
-        // if(a - coins[i] >= 0)
-        // {
-        //     pick =  fn(i, a - coins[i], coins, dp) + 1;
-        // }
+        if(a - coins[i] >= 0)
+        {
+            int res = fn(i, a - coins[i], coins, dp);
 
-        // return dp[i][a] = min(pick, notpick);
+            if(res < INT_MAX) pick = res + 1;
+        }
+
+        return dp[i][a] = min(pick, notpick);
         
-    // }
+    }
 
     int coinChange(vector<int>& coins, int amount) {
-        // int n = coins.size();
+        int n = coins.size();
 
-        // vector<vector<int>> dp(n, vector<int> (amount+2, -1));
+        vector<vector<int>> dp(n, vector<int> (amount+2, -1));
 
-        // int ans = fn(n-1, amount, coins, dp);
+        int ans = fn(n-1, amount, coins, dp);
 
-        // if(ans == 1000000) return -1;
+        if(ans == INT_MAX) return -1;
 
-        // return ans;
+        return ans;
 
 // ----------------------------------------------------------------
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+2, amount+1));
+        // int n = coins.size();
+        // vector<vector<int>> dp(n, vector<int> (amount+2, amount+1));
 
-        for(int i = 0; i < n; i++)
-        {
-            dp[i][0] = 0;
-        }
+        // for(int i = 0; i < n; i++)
+        // {
+        //     dp[i][0] = 0;
+        // }
 
-        for(int i = 0; i < n; i++)
-        {
-            for(int a = 1; a <= amount; a++)
-            {
-                int notpick = amount + 1;
+        // for(int i = 0; i < n; i++)
+        // {
+        //     for(int a = 1; a <= amount; a++)
+        //     {
+        //         int notpick = amount + 1;
 
-                if(i-1 >= 0) notpick = dp[i-1][a];
+        //         if(i-1 >= 0) notpick = dp[i-1][a];
 
-                int pick = amount + 1;
+        //         int pick = amount + 1;
 
-                int val = a - coins[i];
+        //         int val = a - coins[i];
 
-                if(val >= 0)
-                {
-                    pick =  dp[i][val]%mod + 1;
-                }
+        //         if(val >= 0)
+        //         {
+        //             pick =  dp[i][val]%mod + 1;
+        //         }
 
-                dp[i][a] = min(pick, notpick);                
-            }
-        }
+        //         dp[i][a] = min(pick, notpick);                
+        //     }
+        // }
 
-        if(dp[n-1][amount] >= amount + 1) return -1;
+        // if(dp[n-1][amount] >= amount + 1) return -1;
 
-        return dp[n-1][amount];
+        // return dp[n-1][amount];
 
 // -----------------------------------------------------------------------------
 
