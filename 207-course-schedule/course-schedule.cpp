adca28c77ec5d935ffe8f052f790ebaba5f76113
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-07 01:12:26 -0800

    Time: 4 ms (71.68%) | Memory: 19.5 MB (63.10%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
-    bool DFS(int i, vector<int> &vis, vector<int> &path_vis, vector<vector<int>> &adj)
+    // true means no cycle
+    // false means there is cycle and not topologically sortable
+    bool topo_sort_dfs(int i, vector<vector<int>>& adj, vector<int>& vis, vector<int>& path_vis)
     {
         vis[i] = 1;
         path_vis[i] = 1;
-        bool flag = false;
+
         for(auto a : adj[i])
         {
             if(vis[a] == 0)
             {
-                if(DFS(a, vis, path_vis, adj)) return true;
+                if(!topo_sort_dfs(a, adj, vis, path_vis))
+                {
+                    return false;
+                }
             }
-            else if(vis[a] != 0 && path_vis[a] == 1)
+            else if(vis[a] == 1)
             {
-                return true;
+                if(path_vis[a] == 1)
+                {
+                    return false;
+                }
             }
         }
 
         path_vis[i] = 0;
-
-        return flag;
+        return true;
     }
 
-    bool canFinish(int numCourses, vector<vector<int>>& x) {
-        int n = numCourses;
-        vector<int> vis(n, 0);
-        vector<int> path_vis(n, 0);
-
+    bool canFinish(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
 
-        for(int i = 0; i < x.size(); i++)
+        for(int i = 0; i < edges.size(); i++)
         {
-            int a = x[i][0];
-            int b = x[i][1];
+            int a = edges[i][0];
+            int b = edges[i][1];
 
             adj[b].push_back(a);
         }
 
-        bool flag = false;
+        vector<int> vis(n, 0);
+        vector<int> path_vis(n,0);
 
         for(int i = 0; i < n; i++)
         {
-            if(vis[i] == 0)
-            {
-                flag = flag || DFS(i, vis, path_vis, adj); 
-                // Think of DFS to see whether or not a cycle exists
-            }
+            if(vis[i] == 0 && !topo_sort_dfs(i,adj,vis,path_vis)) return false;
         }
 
-        return !flag;
+        return true;
     }
 };
