25dacdc29f56c969aa605ce13f79eaceee95f46d
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-07 02:28:24 -0800

    Time: 0 ms (100.00%) | Memory: 12.1 MB (37.38%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+
+        int mn = INT_MAX;
+        string shortest;
+        for(auto a : strs)
+        {
+            if(a.size() < mn)
+            {
+                mn = a.size();
+                shortest = a;
+            }
+        }
+        
+        for(int i = 0; i < mn; i++)
+        {
+            char h = shortest[i];
+            bool flag = true;
+            for(int j = 0; j < strs.size(); j++)
+            {
+                if(strs[j][i] == h) continue;
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag == false)
+            {
+                return shortest.substr(0, i);
+            }
+        }
+
+        return shortest;
+    }
+};
