a60c9793ec12f7a9a3249e4b8bb2a840e4ddf1c5
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-07 18:14:34 -0800

    Time: 0 ms (100.00%) | Memory: 8.1 MB (78.84%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,55 +1,11 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        bool flag = true;
-        for(auto a : word)
-        {
-            if(!isupper(a))
-            {
-                flag = false;
-                break;
-            }
-        }
-
-        if(flag) return true;
-
-        flag = true;
-
-        for(auto a : word)
-        {
-            if(isupper(a))
-            {
-                flag = false;
-                break;
-            }
-        }
-
-        if(flag) return true;
-
-        flag = true;
-
-        for(int i = 0; i < word.size(); i++)
-        {
-            if(i == 0)
-            {
-                if(!isupper(word[i]))
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            else
-            {
-                if(isupper(word[i]))
-                {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-
-        if(flag) return true;
-
-        return false;
+        int count = 0;
+        for (char c : word)
+            if (isupper(c)) count++;
+        
+        return count == word.size() || count == 0 || (count == 1 && isupper(word[0]));
     }
+
 };
