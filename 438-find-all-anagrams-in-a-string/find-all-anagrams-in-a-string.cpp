f9118364006690d4bc3a30084f2f386ed435324d
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-08 11:23:30 -0800

    Time: 3 ms (80.99%) | Memory: 11.7 MB (41.36%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,56 +1,38 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> target(26, 0);
+        if(p.size() > s.size()) return {};
+
+        vector<int> target(26);
 
         for(auto a : p)
         {
             target[a - 'a']++;
         }
 
-        vector<vector<int>> prefix(s.size(), vector<int>(26, 0));
+        vector<int> window(26);
 
-        vector<int> temp(26, 0);
-
-        for(int i = 0; i < s.size(); i++)
+        for(int i = 0; i < p.size(); i++)
         {
-            temp[s[i] - 'a']++;
-            prefix[i] = temp;
+            window[s[i] - 'a']++;
         }
 
         vector<int> ans;
 
-        int n = p.size();
+        int l = 0;
+        int r = l + p.size() - 1;
 
-        for(int i = 0; i+n-1 < s.size(); i++)
+        while(r+1 < s.size())
         {
-            vector<int> diff(26);
-            vector<int> a = prefix[i+n-1];
-            
+            if(window == target) ans.push_back(l);
 
-            // for(auto b : a) cout << b << " ";
-            // cout << endl;
-            // for(auto c : b) cout << c << " ";
-            // cout << endl;
-            // cout << endl;
-
-            
-
-            if(i == 0)
-            {
-                diff = a;
-            }
-            else
-            {
-                vector<int> b = prefix[i-1];
-                for(int k = 0; k < 26; k++)
-                {
-                    diff[k] = a[k] - b[k];
-                }
-            }
-
-            if(diff == target) ans.push_back(i);
+            window[s[l] - 'a']--;
+            r++;
+            window[s[r] - 'a']++;
+            l++;
         }
+
+        if(window == target) ans.push_back(l);
 
         return ans;
     }
