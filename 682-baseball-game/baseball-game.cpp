0f57be36c23d702c001d8325edfbce83a83801e7
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-08 12:04:06 -0800

    Time: 18 ms (4.50%) | Memory: 12.9 MB (14.44%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    bool isnum(string x)
+    {
+        for(auto a : x)
+        {
+            char y = a;
+            int digit = y - '0';
+
+            if(!(digit >= 0 && digit <= 9)) return false;
+        }
+
+        return true;
+    }
+
+    int calPoints(vector<string>& operations) {
+        stack<int> scores;
+
+        for(auto a : operations)
+        {
+            if(a[0] == '-')
+            {
+                string temp(a.begin()+1, a.end());
+
+                if(isnum(temp)) scores.push(-1 * stoi(temp));
+            }
+            
+            if(isnum(a)) scores.push(stoi(a));
+
+            if(a == "+")
+            {
+                int x = scores.top();
+                scores.pop();
+                int y = scores.top();
+                scores.pop();
+
+                scores.push(y);
+                scores.push(x);
+                scores.push(y+x);
+                
+                cout << "+" << " " << endl;
+                cout << y << " " << x << " " << y+x << " " << endl;
+            }
+
+            if(a == "D")
+            {
+                int x = scores.top();
+                scores.pop();
+
+                scores.push(x);
+                scores.push(x*2);
+                
+                cout << "D" << endl;
+                cout << x << " " << x*2 << " " << endl;
+            }
+
+            if(a == "C")
+            {
+                scores.pop();
+            }
+        }
+        int ans = 0;
+        while(!scores.empty())
+        {
+            ans += scores.top();
+            scores.pop();
+        }
+
+        return ans;
+    }
+};
