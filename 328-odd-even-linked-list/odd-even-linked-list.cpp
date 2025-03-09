c767a9778a2adf09fac6d7f072752f6a50e16f70
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-08 19:10:14 -0800

    Time: 0 ms (100.00%) | Memory: 15.6 MB (28.56%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,131 +11,23 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        // ListNode* z = head;
-        // int count = 0;
-        // while(z != NULL)
-        // {
-        //     count++;
-        //     z = z->next;
-        // }
+        if(head == nullptr) return head;
 
-        // if(count <= 2) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenhead = even;
 
-        // ListNode* org = head;
-
-        // ListNode* temp1 = new ListNode(org->val); // even list
-
-        // ListNode* temp1head = temp1;
-
-        // org = org->next;
-
-        // ListNode* temp2 = new ListNode(org->val); // odd list
-
-        // ListNode* temp2head = temp2;
-
-        // org = org->next;
-
-        // ListNode* x = org;
-
-        // int i = 2;
-
-        // while(x != NULL)
-        // {
-        //     if(i % 2 == 0)
-        //     {
-        //         ListNode* current = new ListNode(x->val);
-        //         temp1->next = current;
-        //         temp1 = temp1->next;
-        //     }
-        //     else
-        //     {
-        //         ListNode* current = new ListNode(x->val);
-        //         temp2->next = current;
-        //         temp2 = temp2->next;
-        //     }
-
-        //     x = x->next;
-        //     i++;
-        // }
-
-        // // while(temp1head != NULL)
-        // // {
-        // //     cout << temp1head->val << " ";
-        // //     temp1head = temp1head->next;
-        // // }
-
-        // // while(temp2head != NULL)
-        // // {
-        // //     cout << temp2head->val << " ";
-        // //     temp2head = temp2head->next;
-        // // }
-
-        // ListNode* y = temp1head;
-
-        // while(y->next != NULL)
-        // {
-        //     y = y->next;
-        // }
-
-        // y->next = temp2head;
-
-        // return temp1head;
-
-        // return org;
-
-        if(head == NULL) return head;
-
-        ListNode* mover = head;
-
-        ListNode* ptr1 = mover;
-
-        mover = mover->next;
-
-        if(mover == NULL) return head;
-
-        ListNode* ptr2 = mover;
-        ListNode* head2 = ptr2;
-
-        mover = mover->next;
-
-        if(mover == NULL) return head;
-
-        bool flag = true; // true means even
-
-        while(mover != NULL)
+        while(even != nullptr && even->next != nullptr)
         {
-            if(flag)
-            {
-                ptr1->next = mover;
-                ptr1 = ptr1->next;
-            }
-            else
-            {
-                ptr2->next = mover;
-                ptr2 = ptr2->next;
-            }
+            odd->next = even->next;
+            odd = odd->next;
 
-            mover = mover->next;
-            flag = !flag;
+            even->next = odd->next;
+            even = even->next;
         }
 
-        // while(head != NULL)
-        // {
-        //     cout << head->val << " ";
-        //     head = head->next;
-        // }
-
-        // while(head2 != NULL)
-        // {
-        //     cout << head2->val << " ";
-        //     head2 = head2->next;
-        // }
-
-        ptr1->next = head2;
-
-        ptr2->next = NULL;
+        odd->next = evenhead;
 
         return head;
-
     }
 };
