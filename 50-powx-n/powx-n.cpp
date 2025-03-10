0b3331c40d5994550d5ddc0a4ecb7a7a6c14bf5e
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-10 13:51:47 -0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.65%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    double fn(double x, int n)
+    double fn(double base, int exp)
     {
-        if(n == 0) return 1;
+        if(exp == 0) return 1.0;
 
         double ans = 1.0;
 
-        double base = x;
-        int exp = n;
-
         if(exp % 2 == 0)
         {
-            ans = ans * fn(base*base, exp/2);
+            ans *= fn(base*base, exp/2.0);
         }
         else
         {
-            ans = ans * base;
-            ans = ans * fn(base*base, exp/2); 
+            ans *= (base * fn(base*base, exp/2.0));
         }
 
         return ans;
     }
 
     double myPow(double x, int n) {
-        double ans = fn(x, n);    
+        double ans = fn(x,n);
 
         if(n < 0) return 1.0/ans;
 
