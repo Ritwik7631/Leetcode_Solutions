e9fefcbde3ad1b9607e11cb5e321cdf30057129b
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-10 16:05:07 -0700

    Time: 0 ms (100.00%) | Memory: 11 MB (9.48%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     int fn(int i, vector<int>& nums, vector<int>& dp)
     {
-        if(i == 0)
-        {
-            return nums[i];
-        }
-
-        if(i == 1) return max(nums[0], nums[1]);
+        if(i < 0) return 0;
 
         if(dp[i] != -1) return dp[i];
 
         // rob
-        int rob = 0;
-        if(i-2 >= 0) rob = nums[i] + fn(i-2, nums,dp);
+        int rob = nums[i] + fn(i-2, nums,dp);
 
         // no rob
-        int norob = 0;
-        if(i-1 >= 0) norob = fn(i-1, nums,dp);
+        int norob = fn(i-1, nums,dp);
 
         return dp[i]=max(rob, norob);
     }
