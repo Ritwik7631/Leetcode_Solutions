d3e49122c0d1d3f293de12ea9cc2ebf70206d749
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-10 15:35:01 -0700

    Time: 24 ms (19.21%) | Memory: 14.4 MB (16.87%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-    int fn(int i, int j, string &a, string &b, vector<vector<int>> &dp)
+    int fn(int i, int j, string& word1, string& word2, vector<vector<int>>& dp)
     {
-        if(j < 0) return i+1;
+        if(j < 0)
+        {
+            return i+1; // we have to delete everything in word1 to convert
+        }
 
-        if(i < 0) return j+1;
+        if(i < 0)
+        {
+            // if word1 is completely processed but there are still some letters left in word2
+            // the only thing to do here is to keep inserting
+            return j+1;
+        }
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(a[i] == b[j])
-        {
-            return dp[i][j] = fn(i-1, j-1, a, b,dp);
-        }
-        // insertion, deletion, replaction
-        return dp[i][j] = min({1 + fn(i, j-1, a, b,dp), 1 + fn(i-1, j, a, b,dp), 1 + fn(i-1, j-1, a, b,dp)});
+        int a = 0;
+        // insert
+        a = 1 + fn(i, j-1, word1, word2, dp);
+        
+        int b = 0;
+        // delete
+        b = 1 + fn(i-1, j, word1, word2, dp);
+
+        int c = 0;
+        // replace
+        c = 1 + fn(i-1,j-1,word1,word2, dp);
+
+        int d = INT_MAX;
+        // do nothing
+        if(word1[i] == word2[j]) d = 0 + fn(i-1,j-1,word1, word2, dp);
+
+        return dp[i][j]=min({a,b,c,d});
     }
 
-    int minDistance(string a, string b) {
-        int n = a.size();
-        int m = b.size();
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
 
-        vector<vector<int>> dp(n, vector<int> (m, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
 
-        return fn(n-1, m-1, a, b, dp);
+        return fn(n-1, m-1, word1, word2,dp);
     }
 };
