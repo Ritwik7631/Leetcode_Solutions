935c13109dc0c5860fc52cdc2c966e6025c05618
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-10 21:36:10 -0700

    Time: 4 ms (71.53%) | Memory: 19.4 MB (67.72%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,51 +1,50 @@
 class Solution {
 public:
-    // true means no cycle
-    // false means there is cycle and not topologically sortable
-    bool topo_sort_dfs(int i, vector<vector<int>>& adj, vector<int>& vis, vector<int>& path_vis)
+    bool dfs(int i, vector<int>& vis, vector<int>& path_vis, vector<vector<int>>& adj)
     {
         vis[i] = 1;
         path_vis[i] = 1;
 
         for(auto a : adj[i])
         {
-            if(vis[a] == 0)
+            if(!vis[a])
             {
-                if(!topo_sort_dfs(a, adj, vis, path_vis))
-                {
-                    return false;
-                }
+                if(!dfs(a, vis, path_vis, adj)) return false;
             }
-            else if(vis[a] == 1)
+            else
             {
                 if(path_vis[a] == 1)
                 {
-                    return false;
+                    return false;;
                 }
-            }
+            }       
         }
 
         path_vis[i] = 0;
+
         return true;
     }
 
-    bool canFinish(int n, vector<vector<int>>& edges) {
+    bool canFinish(int n, vector<vector<int>>& prereq) {
         vector<vector<int>> adj(n);
 
-        for(int i = 0; i < edges.size(); i++)
+        for(int i = 0; i < prereq.size(); i++)
         {
-            int a = edges[i][0];
-            int b = edges[i][1];
+            int a = prereq[i][0];
+            int b = prereq[i][1];
 
             adj[b].push_back(a);
         }
 
-        vector<int> vis(n, 0);
-        vector<int> path_vis(n,0);
+        vector<int> vis(n);
+        vector<int> path_vis(n);
 
         for(int i = 0; i < n; i++)
         {
-            if(vis[i] == 0 && !topo_sort_dfs(i,adj,vis,path_vis)) return false;
+            if(vis[i] == 0)
+            {
+                if(!dfs(i, vis, path_vis, adj)) return false;
+            }
         }
 
         return true;
