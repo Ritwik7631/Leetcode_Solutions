ab1e0d238fb57cad7bd2d091b6416c7aebb1f027
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-10 18:29:25 -0700

    Time: 0 ms (100.00%) | Memory: 9.8 MB (16.60%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    bool fn(int i, int j, string &p, string &s) {
+    bool fn(int i, int j, string &p, string &s, vector<vector<int>>& dp) {
         // Base cases
         if(i < 0 && j < 0) return true;         // Both pattern and string are exhausted.
         if(i < 0 && j >= 0) return false;        // Pattern is empty but string is not.
         if(j < 0) {                            // String is empty.
             // The remaining pattern must form valid "x*" pairs to match an empty string.
             if(i > 0 && p[i] == '*')
-                return fn(i - 2, j, p, s);
+                return fn(i - 2, j, p, s, dp);
             return false;
         }
+
+        if(dp[i][j] != -1) return dp[i][j];
         
         // Direct match or '.' wildcard.
         if(p[i] == s[j] || p[i] == '.')
-            return fn(i - 1, j - 1, p, s);
+            return dp[i][j] = fn(i - 1, j - 1, p, s, dp);
         
         // Handling '*' operator.
         if(p[i] == '*')
-            return fn(i - 2, j, p, s) ||
-                ((p[i - 1] == s[j] || p[i - 1] == '.') && fn(i, j - 1, p, s));
+            return dp[i][j] = fn(i - 2, j, p, s, dp) ||
+                ((p[i - 1] == s[j] || p[i - 1] == '.') && fn(i, j - 1, p, s, dp));
         
-        return false;
+        return dp[i][j] = false;
     }
 
     bool isMatch(string s, string p) {
         int n = p.size();
         int m = s.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
 
-        return fn(n-1, m-1, p, s);
+        return fn(n-1, m-1, p, s, dp);
     }
 };
