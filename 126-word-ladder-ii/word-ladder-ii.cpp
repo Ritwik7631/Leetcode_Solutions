a650873dd4363da154b094a343666fd4ed799747
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-03-12 20:36:15 -0700

    Time: 11 ms (80.27%) | Memory: 13.7 MB (46.44%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.cpp
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -1,84 +1,91 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    vector<string> temp;
-    void DFS(string current, string stop, unordered_map<string, int> &mp)
-    {
-        temp.push_back(current);
-
-        if(current == stop)
+    void dfs(set<vector<string>>& result, unordered_map<string, vector<string>>& parents, vector<string>& path, string& beginWord, string& current)
+    {        
+        if(current == beginWord)
         {
-            reverse(temp.begin(), temp.end());
-            ans.push_back(temp);
-            reverse(temp.begin(), temp.end());
-            temp.pop_back();
+            vector<string> completepath = path;
+            reverse(completepath.begin(), completepath.end());
+            result.insert(completepath);
             return;
         }
-
-        string dupl = current;
-
-        for(int i = 0; i < current.size(); i++)
+        
+        for(auto parent : parents[current])
         {
-            char original = current[i];
-
-            for(char a = 'a'; a <= 'z'; a++)
-            {
-                current[i] = a;
-
-                if(mp[current] + 1 == mp[dupl])
-                {
-                    DFS(current, stop, mp);
-                }
-            }
-            current[i] = original;
+            path.push_back(parent);
+            dfs(result, parents, path, beginWord, parent);
+            path.pop_back();
         }
-
-        temp.pop_back();
-
-        return;
     }
 
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        set<string> st(wordList.begin(), wordList.end());
-
-        queue<string> q;
-
-        q.push(beginWord);
-
-        st.erase(beginWord);
-
-        unordered_map<string, int> mp;
-
-        mp[beginWord] = 1;
-
-        while(!q.empty() && q.front() != endWord)
+        unordered_set<string> wordSet(wordList.begin(), wordList.end()); // visited
+  
+        if(wordSet.find(endWord) == wordSet.end()) return {};
+        
+        wordSet.erase(beginWord);
+        
+        unordered_map<string, vector<string>> parents;
+        
+        unordered_set<string> current{beginWord};
+        
+        unordered_set<string> next;
+        
+        bool found = false;
+        
+        while(!current.empty() && !found)
         {
-            string word = q.front();
-            string dupl = word;
-            q.pop();
-
-            for(int i = 0; i < word.size(); i++)
+            for(auto a : current)
             {
-                char original = word[i];
-
-                for(char a = 'a'; a <= 'z'; a++)
-                {
-                    word[i] = a;
-
-                    if(st.count(word) == 1)
-                    {
-                        st.erase(word);
-
-                        mp[word] = mp[dupl] + 1;
-
-                        q.push(word);
-                    }
-                }
-                word[i] = original;
+                wordSet.erase(a);
             }
+            
+            next.clear();
+            
+            for(auto word : current)
+            {
+                string temp = word;
+            
+                for(int i = 0; i < word.size(); i++)
+                {
+                    char original = temp[i];
+                    
+                    for(char a = 'a'; a <= 'z'; a++)
+                    {
+                        temp[i] = a;
+                    
+                        if(wordSet.count(temp))
+                        {
+                            next.insert(temp);
+                            parents[temp].push_back(word);
+                        }
+                        
+                        if(temp == endWord)
+                        {
+                            found = true;
+                            parents[temp].push_back(word);
+                        }
+                    }
+                    
+                    temp[i] = original;
+                }
+            }
+            
+            current.swap(next);
         }
+        
+        set<vector<string>> result;
 
-        DFS(endWord, beginWord, mp);
+        vector<vector<string>> temp;
+
+        if(found == false) return temp;
+        
+        vector<string> path = {endWord};
+        dfs(result, parents, path, beginWord, endWord);
+        
+        vector<vector<string>> ans;
+
+        for(auto a : result) ans.push_back(a);
 
         return ans;
     }
