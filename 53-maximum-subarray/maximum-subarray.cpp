b13ee6a043472f044d65d5887dd07a74cf1c7a75
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-04-10 01:22:57 +0600

    Time: 25 ms (5.31%) | Memory: 72.9 MB (5.88%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int max_sum = INT_MIN;
+    int divide(int l, int r, vector<int>& nums)
+    {
+        if(l == r) return nums[l];
 
-        int current_sum = 0;
+        int mid = l + (r-l)/2;
 
-        for(auto a : nums)
+        int left = divide(l, mid, nums);
+        int right = divide(mid+1, r, nums);
+        int combined = fn(nums, l, r, mid);
+
+        return max({left, right, combined});
+    }
+
+    int fn(vector<int>& nums, int l, int r, int mid)
+    {
+        int max_left = INT_MIN;
+        int curr = 0;
+        for(int i = mid; i >= l; i--)
         {
-            current_sum += a;
-
-            max_sum = max(max_sum, current_sum);
-
-            if(current_sum < 0)
-            {
-                current_sum = 0;
-            }
+            curr += nums[i];
+            max_left = max(max_left, curr);
         }
 
+        int max_right = INT_MIN;
+        curr = 0;
+        for(int i = mid+1; i <= r; i++)
+        {
+            curr += nums[i];
+            max_right = max(max_right, curr);
+        }
 
-        return max_sum;
+        return max_right + max_left;
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        return divide(0, nums.size()-1, nums);
     }
 };
