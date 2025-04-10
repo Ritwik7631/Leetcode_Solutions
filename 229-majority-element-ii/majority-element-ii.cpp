050444db15423b91adde6b2ead275397863d9765
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-04-10 09:51:13 +0600

    Time: 0 ms (100.00%) | Memory: 19.9 MB (60.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,6 +3,8 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
 
+        // 
+
         int element1 = INT_MIN, element2 = INT_MIN;
         int count1 = 0, count2 = 0;
 
@@ -10,13 +12,13 @@ public:
         {
             if(count1 == 0 && nums[i] != element2)
             {
-                count1 = 1;
                 element1 = nums[i];
+                count1++;
             }
             else if(count2 == 0 && nums[i] != element1)
             {
-                count2 = 1;
                 element2 = nums[i];
+                count2++;
             }
             else if(nums[i] == element1)
             {
@@ -32,19 +34,26 @@ public:
                 count2--;
             }
         }
+
         count1 = 0;
         count2 = 0;
+
         for(int i = 0; i < n; i++)
         {
-            if(nums[i] == element1) count1++;
-            if(nums[i] == element2) count2++;
+            if(element1 == nums[i]) count1++;
+            if(element2 == nums[i]) count2++;
         }
-        vector<int> ans;
+
+        cout << element1 << " " << element2 << endl;
+
+        set<int> ans;
+
+        if(count1 >= n/3 + 1) ans.insert(element1);
+
+        if(count2 >= n/3 + 1) ans.insert(element2);
+
+        return vector<int>(ans.begin(), ans.end());
+
         
-        if(count1 >= n/3 + 1) ans.push_back(element1);
-
-        if(count2 >= n/3 + 1) ans.push_back(element2);
-
-        return ans;
     }
 };
