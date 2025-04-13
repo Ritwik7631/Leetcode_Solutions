684af672acf5623ee4c5bd48162c9b5846737a4a
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-04-13 12:21:21 +0600

    Time: 0 ms (100.00%) | Memory: 94.9 MB (93.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,37 +1,63 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>& A = nums1;
-        vector<int>& B = nums2;
-        int total = A.size() + B.size();
-        int half = (total + 1) / 2;
+        /*
+        
+        1 7 8 9 12 34 56                    10 13 24 76
+            l r                    l           lr
+            m                                   m           
 
-        if (B.size() < A.size()) {
-            swap(A, B);
+        
+        0 1 2 3 4 5 6 7 8 9 10
+        
+        */
+
+        int n = nums1.size();
+        int m = nums2.size();
+
+        if(m < n)
+        {
+            return findMedianSortedArrays(nums2, nums1);
         }
+
+        int total = n + m;
 
         int l = 0;
-        int r = A.size();
-        while (l <= r) {
-            int i = (l + r) / 2;
-            int j = half - i;
+        int r = n;
 
-            int Aleft = i > 0 ? A[i - 1] : INT_MIN;
-            int Aright = i < A.size() ? A[i] : INT_MAX;
-            int Bleft = j > 0 ? B[j - 1] : INT_MIN;
-            int Bright = j < B.size() ? B[j] : INT_MAX;
+        while(l <= r)
+        {
+            int partition1 = l + (r-l)/2;
 
-            if (Aleft <= Bright && Bleft <= Aright) {
-                if (total % 2 != 0) {
-                    return max(Aleft, Bleft);
+            int partition2 = (total+1)/2 - partition1;
+
+            int max1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
+            int min1 = (partition1 == n) ? INT_MAX : nums1[partition1];
+
+            int max2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
+            int min2 = (partition2 == m) ? INT_MAX : nums2[partition2];
+
+            if(max1 <= min2 && max2 <= min1)
+            {
+                if(total%2 == 1)
+                {
+                    return max(max1, max2);
                 }
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2.0;
-            } else if (Aleft > Bright) {
-                r = i - 1;
-            } else {
-                l = i + 1;
+                else
+                {
+                    return (max(max1,max2) + min(min1, min2)) / 2.0;
+                }
+            }
+            else if(max1 > min2)
+            {
+                r = partition1 - 1;
+            }
+            else
+            {
+                l = partition1 + 1;
             }
         }
-        return -1;
+
+        return 0.0;
     }
 };
