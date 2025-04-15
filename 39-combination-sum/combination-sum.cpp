ff7cbbb4171a3ac1988892239cbc4170670633eb
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-04-15 16:05:36 -0700

    Time: 4 ms (44.00%) | Memory: 15.5 MB (38.58%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    
-    void dfs(vector<int>& candidates, int target, int start, vector<int>& curr) {
-        if(target == 0) {
-            res.push_back(curr);
+    void fn(int i, vector<int>& candidates, int rem, vector<int>& temp, set<vector<int>>& result)
+    {
+        if(rem == 0)
+        {
+            vector<int> sorted_temp = temp; // we have to create a new vector because if we modify the original temp then we will run into problems when we backtrack.
+            result.insert(sorted_temp);
             return;
         }
+
+        if(i < 0) return;
         
-        for(int i = start; i < candidates.size(); i++){
-            if(candidates[i] > target) break;
-            curr.push_back(candidates[i]);
-            dfs(candidates, target - candidates[i], i, curr);
-            curr.pop_back(); 
+        // pick
+        if(rem - candidates[i] >= 0)
+        {
+            temp.push_back(candidates[i]);
+            fn(i, candidates, rem - candidates[i], temp, result);
+            temp.pop_back();
         }
+
+        // not pick
+        fn(i-1, candidates, rem, temp, result);
     }
-    
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<int> curr;
-        dfs(candidates, target, 0, curr);
-        return res;
+        int n = candidates.size();
+        vector<int> temp;
+
+        set<vector<int>> result;
+        
+        fn(n-1, candidates, target, temp, result);
+
+        vector<vector<int>> ans(result.begin(), result.end());
+        return ans;
     }
 };
