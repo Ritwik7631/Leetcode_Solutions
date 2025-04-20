38ea44484055bd693e56781239874e08edefdca0
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-04-20 12:28:47 -0700

    Time: 11 ms (95.15%) | Memory: 81.3 MB (71.57%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,37 +1,55 @@
-#include <vector>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        /*
-        the idea is to use a stack to store the index of the heights in the histogram. The stack is monotonically increasing. Then we take the minimum of the monotonically
-        increasing stack and that is our height and then we see how many bars contribute to the width
-        */
+        int ans = 0;
 
-       int n = heights.size();
-       stack<int> stk;
-       int mx = 0;
-       for(int i = 0; i < n; i++)
-       {
+        stack<int> stk;
+
+        int n = heights.size();
+
+        for(int i = 0; i < n; i++)
+        {
             while(!stk.empty() && heights[i] < heights[stk.top()])
             {
                 int height = heights[stk.top()];
                 stk.pop();
-                int width = stk.empty() ? i : i - stk.top() - 1; // if stk is empty then the width is the current index else the width is the current index - the index of the top of the stack
-                mx = max(mx, height * width);
-            }
-            stk.push(i);
-       }
 
-       while(!stk.empty())
-       {
-        int height = heights[stk.top()];
-        stk.pop();
-        int width = stk.empty() ? n : n - stk.top() - 1;
-        mx = max(mx, height * width);
-       }
-       return mx;
+                int width;
+
+                if(stk.empty())
+                {
+                    width = i;
+                }
+                else
+                {
+                    width = i - stk.top() - 1;
+                }
+
+                ans = max(ans, height * width);
+            }
+
+            stk.push(i);
+        }
+
+        while(!stk.empty())
+        {
+            int height = heights[stk.top()];
+            stk.pop();
+
+            int width;
+
+            if(stk.empty())
+            {
+                width = n;
+            }
+            else
+            {
+                width = n - 1 - stk.top();
+            }
+
+            ans = max(ans, height * width);
+    }
+
+        return ans;
     }
 };
