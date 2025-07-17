6b979f253da1d134737fac099f6dfecd17805556
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-07-16 20:41:30 -0700

    Time: 27 ms (92.15%) | Memory: 14.4 MB (99.15%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,42 +1,47 @@
 class Solution {
-public:
-    int dist(vector<int> &a, vector<int> &b){
-        return abs(a[0] - b[0]) + abs(a[1] - b[1]);
-    }
 
+int dist(vector<int> &a, vector<int> &b){
+    return abs(b[0] - a[0]) + abs(b[1] - a[1]);
+}
+
+public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
 
-        vector<int> min_weight(n, 1e9);
-        vector<int> parent(n, -1);
+        vector<int> min_weight(n, INT_MAX);
         vector<bool> inMST(n, false);
 
         min_weight[0] = 0;
-        int ans = 0;
+
+        int result = 0;
+
 
         for(int i = 0; i < n; i++){
+            // instead of making use of priority queue I can just loop to find the edge with smallest weight
             int u = -1;
+            int best = INT_MAX;
 
             for(int j = 0; j < n; j++){
-                if(!inMST[j] && (u == -1 || min_weight[j] < min_weight[u])){
+                if(!inMST[j] && min_weight[j] < best){
+                    best = min_weight[j];
                     u = j;
                 }
             }
 
             inMST[u] = true;
-            ans += min_weight[u];
+
+            result += min_weight[u];
 
             for(int v = 0; v < n; v++){
-                if(!inMST[v]){
+                if(inMST[v] == false){
                     int w = dist(points[u], points[v]);
                     if(w < min_weight[v]){
                         min_weight[v] = w;
-                        parent[v] = u;
                     }
                 }
             }
         }
 
-        return ans;
+        return result;
     }
 };
