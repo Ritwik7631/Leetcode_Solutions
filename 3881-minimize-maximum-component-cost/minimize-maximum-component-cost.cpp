af91f39e3842b7acd340f537180b8513140800e3
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-07-16 20:10:55 -0700

    Time: 277 ms (44.87%) | Memory: 291.8 MB (24.85%) - LeetSync

--- 3881-minimize-maximum-component-cost/minimize-maximum-component-cost.cpp
+++ 3881-minimize-maximum-component-cost/minimize-maximum-component-cost.cpp
@@ -1,58 +1,64 @@
 class Solution {
 public:
     int minCost(int n, vector<vector<int>>& edges, int k) {
+        // construct graph
+        // use prim's algoritm to find the edges that are part of minimum spanning tree
+        // sort the edges in descending order
+        // pick the edge at the k-1 index, we are logically removing the edges that come prior
+
         vector<vector<pair<int,int>>> adj(n);
 
         for(int i = 0; i < edges.size(); i++){
             int u = edges[i][0];
             int v = edges[i][1];
             int w = edges[i][2];
-
             adj[u].push_back({v,w});
             adj[v].push_back({u,w});
         }
 
-        vector<int> min_weight(n, 1e9), parent(n, -1); 
+        vector<int> min_weight(n, INT_MAX), parent(n, -1);
         vector<bool> inMST(n, false);
+        
 
-        multiset<pair<int,int>> ms; // weight, node
-
-        min_weight[0] = 0;
+        multiset<pair<int,int>> ms;
         ms.insert({0,0});
+        min_weight[0] = 0;
 
         while(!ms.empty()){
-            auto it = ms.begin();
-            int u = it->second;
+            auto it = *ms.begin();
             ms.erase(it);
+            int u = it.second;
 
             if(inMST[u]) continue;
+
             inMST[u] = true;
 
-            for(auto &it : adj[u]){
-                int v = it.first;
-                int wt = it.second;
-                if(!inMST[v] && wt < min_weight[v]){
-                    if(min_weight[v] != 1e9) ms.erase(ms.find({min_weight[v], v}));
-                    min_weight[v] = wt;
+            for(auto a : adj[u]){
+                int v = a.first;
+                int w = a.second;
+
+                if(!inMST[v] && w < min_weight[v]){
+                    if(min_weight[v] != INT_MAX) ms.erase(ms.find({min_weight[v],v}));
+
+                    min_weight[v] = w;
                     parent[v] = u;
-                    ms.insert({wt, v});
+                    ms.insert({w, v});
                 }
             }
         }
 
-        vector<int> mstW;
+        vector<int> MST_Weights;
 
-        for(int v = 1; v < n; v++){
-            if(parent[v] != -1){
-                mstW.push_back(min_weight[v]);
+        for(int i = 1; i < n; i++){
+            if(parent[i] != -1){
+                MST_Weights.push_back(min_weight[i]);
             }
         }
 
-        sort(mstW.begin(), mstW.end(), greater<int>());
-        // number of removals is k-1 if u want k components
-        // if number of edge removals >= the number of edges in MST then return 0
-        if(k - 1 >= (int)mstW.size()) return 0;
+        sort(MST_Weights.begin(), MST_Weights.end(), greater<int>());
 
-        return mstW[k-1];
-    }
+        if(k-1 >= MST_Weights.size()) return 0;
+
+        return MST_Weights[k-1];
+    }   
 };
