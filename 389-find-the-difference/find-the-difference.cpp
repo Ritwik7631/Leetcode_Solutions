929f437a693ed424167112c985f02ef02e6ce5f4
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-09-26 06:39:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.46%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int> mp1;
-        unordered_map<char,int> mp2;
+        char ans = 0;
 
-        for(auto a : s) mp1[a]++;
-        for(auto a : t) mp2[a]++;
+        for(auto a : s) ans ^= a;
+        for(auto a : t) ans ^= a;
 
-        for(auto a : t)
-        {
-            if(mp2[a]-mp1[a] == 1) return a;
-        }
-
-        return 'c';
+        return ans;
     }
 };
