95d0299ed6a019eb8cc8969543a3672b022077b0
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-09-26 08:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (13.11%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
+    vector<int> prefix_function(string& P){
+        int m = P.size();
+        vector<int> pi(m, 0);
+        int k = 0;
+
+        for(int q = 1; q < m; q++){
+            while(k > 0 && P[k] != P[q]) k = pi[k-1];
+
+            if(P[k] == P[q]) k++;
+
+            pi[q] = k;
+        }
+
+        return pi;
+    }
+
     int strStr(string haystack, string needle) {
         int n = needle.size();
         int m = haystack.size();
 
-        for(int i = 0; i < m; i++){
-            string potential = haystack.substr(i, n);
+        if(n == 0) return 0;
 
-            if(potential == needle) return i;
+        vector<int> pi = prefix_function(needle);
+
+        int j = 0;
+
+        for(int i = 0; i < m; i++){
+            while(j > 0 && needle[j] != haystack[i]) j = pi[j-1];
+
+            if(needle[j] == haystack[i]) j++;
+
+            if(j == n) return i - n + 1;
         }
 
         return -1;
