110698e99ab4d0fcbe8addc3889517af931e2582
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-09-30 10:03:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (48.00%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    int signFunc(int& x){
-        if(x > 0) return 1;
-        else if(x < 0) return -1;
-
-        return 0;
-    }
-
     int arraySign(vector<int>& nums) {
-        int val = 1;
-        for(auto a : nums) val *= signFunc(a);
-
-        return signFunc(val);
+        int negCount = 0;
+        for (int x : nums) {
+            if (x == 0) return 0;
+            if (x < 0) negCount++;
+        }
+        return (negCount % 2 == 0) ? 1 : -1;
     }
 };
