6163b09a0354c82fa2c82f8324a92aba049e11a3
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-10-03 02:16:55 +0530

    Time: 7 ms (51.54%) | Memory: 12.3 MB (68.57%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        static const int lut[256] = {
-            /* 'C' */ ['C'] = 100,
-            /* 'D' */ ['D'] = 500,
-            /* 'I' */ ['I'] = 1,
-            /* 'L' */ ['L'] = 50,
-            /* 'M' */ ['M'] = 1000,
-            /* 'V' */ ['V'] = 5,
-            /* 'X' */ ['X'] = 10
-        };
+        unordered_map<char,int> mp;
 
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        int n = s.size();
         int ans = 0;
-        for (size_t i = 0; i < s.size(); ++i) {
-            int v = lut[(unsigned char)s[i]];
-            if (i + 1 < s.size() && v < lut[(unsigned char)s[i + 1]]) ans -= v;
-            else ans += v;
+        for(int i = 0; i < n; i++){
+            int val = mp[s[i]];
+
+            if(i+1 < n && val < mp[s[i+1]]) ans -= val;
+            else ans += val;
         }
+
         return ans;
     }
 };
