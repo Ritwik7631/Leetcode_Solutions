bab10c9566374569cb77b65bcfa65ea8c1382e4d
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-10-04 09:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (78.22%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -5,26 +5,30 @@ public:
 
         int n = operations.size();
 
+        int ans = 0;
+
         for(int i = 0; i < n; i++){
             if(operations[i] == "C"){
+                ans -= record[record.size()-1];
                 record.pop_back();
             }
             else if(operations[i] == "D"){
                 int n1 = record[record.size()-1];
                 record.push_back(n1*2);
+                ans += (n1*2);
             }
             else if(operations[i] == "+"){
                 int n1 = record[record.size()-1];
                 int n2 = record[record.size()-1-1];
                 int n3 = n1+n2;
                 record.push_back(n3);
+                ans += n3;
             }
             else{
                 record.push_back(stoi(operations[i]));
+                ans += stoi(operations[i]);
             }   
         }
-
-        int ans = accumulate(record.begin(), record.end(), 0);
 
         return ans;
     }
