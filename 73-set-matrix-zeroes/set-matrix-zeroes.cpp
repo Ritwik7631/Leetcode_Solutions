99cbb8c3fd7ee749b1e688051977a4aed63b448a
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-10-09 14:21:08 -0700

    Time: 3 ms (10.57%) | Memory: 19 MB (5.61%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,84 +1,35 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
-        // int n = matrix.size();
-        // int m = matrix[0].size();
+        vector<int> rdel = {0, -1, 0, 1};
+        vector<int> cdel = {1, 0, -1, 0};
 
-        // vector<int> row(n, 0);
-        // vector<int> col(m, 0);
+        // right, down, left, up
 
-        // for(int i = 0; i < n; i++)
-        // {
-        //     for(int j = 0; j < m; j++)
-        //     {
-        //         if(matrix[i][j] == 0)
-        //         {
-        //             row[i] = 1;
-        //             col[j] = 1;
-        //         }   
-        //     }
-        // }
-
-        // for(int j = 0; j < m; j++)
-        // {
-        //     for(int i = 0; i < n; i++)
-        //     {
-        //         if(row[i] == 1 || col[j])
-        //         {
-        //             matrix[i][j] = 0;
-        //         }
-        //     }
-        // }
-
-        // return;
-
-// -----------------------------------
         int n = matrix.size();
         int m = matrix[0].size();
 
-        int r = 0;
-        int c = 0;
+        vector<vector<int>> mark(n, vector<int> (m, 0));
 
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(matrix[i][j] == 0)
-                {
-                    if(i == 0) r = 1;
-                    else matrix[i][0] = 0;
-
-                    if(j == 0) c = 1;
-                    else matrix[0][j] = 0;
-                }   
-            }
-        }
-
-        for(int j = 1; j < m; j++)
-        {
-            for(int i = 1; i < n; i++)
-            {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0)
-                {
-                    matrix[i][j] = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k = 0; k < 4; k++){
+                        int new_x = i + rdel[k];
+                        int new_y = j + cdel[k];
+                        while(new_x >= 0 && new_x < n && new_y >= 0 && new_y < m && matrix[new_x][new_y] != 0){
+                            mark[new_x][new_y] = 1;
+                            new_x += rdel[k];
+                            new_y += cdel[k];
+                        }
+                    }
                 }
             }
         }
 
-        if(r)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if(c)
-        {
-            for(int i = 0; i < n; i++)
-            {
-                matrix[i][0] = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mark[i][j]) matrix[i][j] = 0;
             }
         }
 
