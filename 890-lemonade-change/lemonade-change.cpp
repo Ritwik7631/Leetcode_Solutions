b39ff1a4b823ae54f994bcfbf5ab559418a451e7
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-10-15 14:41:40 -0700

    Time: 0 ms (100.00%) | Memory: 87.1 MB (93.43%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n = bills.size();
+
+        int fives = 0;
+        int tens = 0;
+        int twentys = 0;
+
+        for(int i = 0; i < n; i++){
+            if(bills[i] == 5){
+                fives++;
+            }
+            else if(bills[i] == 10){
+                tens++;
+
+                if(fives >= 1) {
+                    fives--;
+                } else {
+                    return false;
+                }
+            }
+            else {
+                twentys++;
+
+                if(tens >= 1 && fives >= 1){
+                    tens--;
+                    fives--;
+                }
+                else if(fives >= 3){
+                    fives -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
