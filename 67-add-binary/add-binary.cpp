776a343a816c84660418c997de898fd4190da84e
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-10-16 13:36:45 -0700

    Time: 1 ms (32.74%) | Memory: 9.1 MB (38.75%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i = a.size() - 1;
-        int j = b.size() - 1;
+        int n = a.size();
+        int m = b.size();
 
+        int i = n-1;
+        int j = m-1;
         int carry = 0;
 
         string ans = "";
 
-        while(i >= 0 || j >= 0 || carry){
-            int x = 0, y = 0;
-
-            if(i >= 0) x = a[i] - '0';
-
-            if(j >= 0) y = b[j] - '0';
+        while(i >= 0 && j >= 0){
+            int x = a[i] - '0';
+            int y = b[j] - '0';
 
             int sum = x + y + carry;
 
             int digit = sum % 2;
+            carry = sum/2;
+
             ans += (digit + '0');
-            carry = sum / 2;
 
             i--;
             j--;
         }
+
+        while(i >= 0){
+            int x = a[i] - '0';
+
+            int sum = x + carry;
+
+            int digit = sum % 2;
+            carry = sum/2;
+
+            ans += (digit + '0');
+
+            i--;
+        }
+
+        while(j >= 0){
+            int y = b[j] - '0';
+
+            int sum = y + carry;
+
+            int digit = sum % 2;
+            carry = sum/2;
+
+            ans += (digit + '0');
+
+            j--;
+        }
+
+        if(carry) ans.push_back(carry + '0');
 
         reverse(ans.begin(), ans.end());
 
