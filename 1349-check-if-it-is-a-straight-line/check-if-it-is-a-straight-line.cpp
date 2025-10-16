597985d06e4cf11fd7af609dc867e5d5c11bb6b9
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-10-15 20:12:58 -0700

    Time: 0 ms (100.00%) | Memory: 14.1 MB (42.12%) - LeetSync

--- 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    double calc_slope(vector<int> a1, vector<int> a2){
-        if (a2[0] == a1[0]) return DBL_MAX;
-        return (double)(a2[1] - a1[1]) / (a2[0] - a1[0]);
-    }
+    bool checkStraightLine(vector<vector<int>>& c) {
+        int n = c.size();
+        if (n <= 2) return true;
 
-    double calc_y_int(double m, vector<int> a){
-        if(m == DBL_MAX) return DBL_MAX;
-        return a[1] - (m*a[0]);
-    }
+        int x0 = c[0][0], y0 = c[0][1];
+        int dx = c[1][0] - x0;
+        int dy = c[1][1] - y0;
 
-    bool valid(double m, double b, int x, int y){
-        if(m == DBL_MAX) return x == b;
-        return y == (m*x) + b;
-    }
-
-    bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int n = coordinates.size();
-
-        double slope = calc_slope(coordinates[0], coordinates[1]);
-
-        double y_int;
-        
-        if(slope == DBL_MAX) y_int = coordinates[0][0];
-        else y_int = calc_y_int(slope, coordinates[0]);
-
-        for(auto a : coordinates){
-            if(!valid(slope, y_int, a[0], a[1])) return false; 
+        for (int i = 2; i < n; i++) {
+            int x = c[i][0], y = c[i][1];
+            if ((x - x0) * dy != (y - y0) * dx) return false;
         }
-
         return true;
     }
 };
