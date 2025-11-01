4d3d762d85d3b22e250a23a7453d25d3028a326a
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-10-31 20:11:19 -0700

    Time: 215 ms (89.41%) | Memory: 208.6 MB (50.75%) - LeetSync

--- 4083-stable-subarrays-with-equal-boundary-and-interior-sum/stable-subarrays-with-equal-boundary-and-interior-sum.cpp
+++ 4083-stable-subarrays-with-equal-boundary-and-interior-sum/stable-subarrays-with-equal-boundary-and-interior-sum.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     long long countStableSubarrays(vector<int>& capacity) {
         int n = capacity.size();
+
+        vector<long long> prefix(n+1, 0);
+
+        for(int i = 0; i < n; i++) prefix[i+1] = prefix[i] + capacity[i];
+
+        unordered_map<long long, unordered_map<long long,long long>> cnt;
+
         long long ans = 0;
 
-        unordered_map<int,vector<int>> last_seen; // value, indices
+        for(int r = 0; r < n; r++){
 
-        vector<long long> prefix(n+1,0);
-
-        for(int i = 0; i < n; i++){
-            prefix[i+1] = prefix[i] + capacity[i];
-        }
-
-        unordered_map<long long, unordered_map<long,long>> cnt;
-
-        for(int i = 0; i < n; i++){
-
-            if(i - 2 >= 0){
-                int a = i-2;
-                cnt[capacity[a]][prefix[a]]++;
+            if(r - 2 >= 0){
+                int l = r-2;
+                cnt[capacity[l]][prefix[l]]++;
             }
 
-            long long need = prefix[i+1] - 3LL*capacity[i];
+            long long need = prefix[r+1] - 3LL * capacity[r];
 
-            if(cnt.count(capacity[i])){
-                auto &inner = cnt[capacity[i]];
-                if(inner.count(need)) ans += inner[need];
+            if(cnt.count(capacity[r])){
+                auto &mp = cnt[capacity[r]];
+                if(mp.count(need)) ans += mp[need];
             }
         }
 
