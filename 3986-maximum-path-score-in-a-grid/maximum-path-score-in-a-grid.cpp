b359d4e14a616cd66bac8a11ea45d70e13451fff
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-11-11 23:21:13 -0800

    Time: 864 ms (52.62%) | Memory: 499.5 MB (48.27%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,60 +1,53 @@
 class Solution {
 public:
-    static constexpr int UNVIS = -2;
-    static constexpr int IMP = -1;
-
-    int n, m;
-    vector<vector<int>> *G;
     vector<vector<vector<int>>> memo;
+    vector<vector<int>> *G;
 
-    int cost(int v){
-        return v == 0 ? 0 : 1;
+    int UNVIS = -1;
+    int IMP = -2;
+
+    int n,m;
+
+    int cost(int x){
+        return (x == 0) ? 0 : 1;
     }
 
     int dfs(int i, int j, int rem){
-        
+        if(i == n-1 && j == m-1) return (*G)[i][j];
+
         if(memo[i][j][rem] != UNVIS) return memo[i][j][rem];
 
-        if(i == n-1 && j == m-1) return memo[i][j][rem] = 0;
-
         int best = IMP;
-        if (i + 1 < n) {
-            int w = cost((*G)[i + 1][j]);
-            if (rem >= w) {
-                int down = dfs(i + 1, j, rem - w);
-                if (down != IMP) {
-                    int cand = (*G)[i + 1][j] + down;
-                    if (cand > best) best = cand;
-                }
+
+        // down
+        if(i+1 < n){
+            int c = cost((*G)[i+1][j]);
+            if(rem - c >= 0){
+                int down = dfs(i+1, j, rem - c);
+                if(down != IMP) best = max(best, (*G)[i][j] + down);
             }
         }
 
-        if (j + 1 < m) {
-            int w = cost((*G)[i][j + 1]);
-            if (rem >= w) {
-                int right = dfs(i, j + 1, rem - w);
-                if (right != IMP) {
-                    int cand = (*G)[i][j + 1] + right;
-                    if (cand > best) best = cand;
-                }
+        if(j+1 < m){
+            int c = cost((*G)[i][j+1]);
+            if(rem - c >= 0){
+                int right = dfs(i, j+1, rem-c);
+                if(right != IMP) best = max(best, (*G)[i][j] + right);
             }
         }
 
         return memo[i][j][rem] = best;
     }
 
-
     int maxPathScore(vector<vector<int>>& grid, int k) {
         n = grid.size();
         m = grid[0].size();
         G = &grid;
+
         memo.assign(n, vector<vector<int>> (m, vector<int> (k+1, UNVIS)));
 
         int ans = dfs(0,0,k);
-        
-        if(ans == IMP) return -1;
 
-        return grid[0][0] + ans;
-
+        return ans == IMP ? -1 : ans;
     }
 };
