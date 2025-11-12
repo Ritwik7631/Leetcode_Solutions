90aa0dbf2126b627675ab20663603ad6bd50ba05
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2025-11-12 13:29:19 -0800

    Time: 923 ms (36.25%) | Memory: 499.4 MB (48.38%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> memo;
+    int n, m;
     vector<vector<int>> *G;
+    vector<vector<vector<int>>> memo;
 
     int UNVIS = -1;
     int IMP = -2;
-
-    int n,m;
 
     int cost(int x){
         return (x == 0) ? 0 : 1;
@@ -19,20 +18,19 @@ public:
 
         int best = IMP;
 
-        // down
-        if(i+1 < n){
-            int c = cost((*G)[i+1][j]);
-            if(rem - c >= 0){
-                int down = dfs(i+1, j, rem - c);
-                if(down != IMP) best = max(best, (*G)[i][j] + down);
-            }
-        }
-
         if(j+1 < m){
             int c = cost((*G)[i][j+1]);
             if(rem - c >= 0){
                 int right = dfs(i, j+1, rem-c);
                 if(right != IMP) best = max(best, (*G)[i][j] + right);
+            }
+        }
+
+        if(i+1 < n){
+            int c = cost((*G)[i+1][j]);
+            if(rem - c >= 0){
+                int down = dfs(i+1, j, rem-c);
+                if(down != IMP) best = max(best, (*G)[i][j] + down);
             }
         }
 
@@ -48,6 +46,6 @@ public:
 
         int ans = dfs(0,0,k);
 
-        return ans == IMP ? -1 : ans;
+        return (ans == IMP) ? -1 : ans;
     }
 };
