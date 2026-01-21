8e381a87b6e1999d0090b5a58a1dfd15622c301a
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2026-01-20 22:49:38 -0800

    Time: 136 ms (95.98%) | Memory: 354.5 MB (73.23%) - LeetSync

--- 4067-design-auction-system/design-auction-system.cpp
+++ 4067-design-auction-system/design-auction-system.cpp
@@ -1,44 +1,51 @@
 class AuctionSystem {
 public:
-    unordered_map<int, unordered_map<int,int>> bids;
-    unordered_map<int, priority_queue<pair<long long,int>>> pq;
-
+    unordered_map<int, unordered_map<int,int>> a; // [item][user] = amount;
+    unordered_map<int, priority_queue<pair<long long, int>>> pq;
     AuctionSystem() {}
-
+    
     void addBid(int userId, int itemId, int bidAmount) {
-        bids[itemId][userId] = bidAmount;
+        a[itemId][userId] = bidAmount;
         pq[itemId].push({bidAmount, userId});
     }
-
+    
     void updateBid(int userId, int itemId, int newAmount) {
-        bids[itemId][userId] = newAmount;
+        a[itemId][userId] = newAmount;
         pq[itemId].push({newAmount, userId});
     }
-
+    
     void removeBid(int userId, int itemId) {
-        bids[itemId].erase(userId);
+        a[itemId].erase(userId);
     }
-
+    
     int getHighestBidder(int itemId) {
-        if (!bids.count(itemId)) return -1;
+        if(a[itemId].empty()) return -1;
 
         auto &heap = pq[itemId];
-        auto &mp   = bids[itemId];
+        auto &mp = a[itemId];
 
-        // Remove stale entries
-        while (!heap.empty()) {
-            auto [amount, userId] = heap.top();
-            if (!mp.count(userId)) {     // removed bid — stale
+        while(!heap.empty()){
+            auto [b, u] = heap.top();
+            if(mp.count(u) == 0){
                 heap.pop();
                 continue;
             }
-            if (mp[userId] != amount) {  // outdated bid — stale
+            else if(b != mp[u]){
                 heap.pop();
                 continue;
             }
-            return userId;               // valid
+            return u;
         }
 
         return -1;
     }
 };
+
+/**
+ * Your AuctionSystem object will be instantiated and called as such:
+ * AuctionSystem* obj = new AuctionSystem();
+ * obj->addBid(userId,itemId,bidAmount);
+ * obj->updateBid(userId,itemId,newAmount);
+ * obj->removeBid(userId,itemId);
+ * int param_4 = obj->getHighestBidder(itemId);
+ */
