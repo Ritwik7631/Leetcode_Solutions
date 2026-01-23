48698507d9f770e27e58c937baadd953b9e5825b
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2026-01-22 21:25:28 -0800

    Time: 213 ms (25.55%) | Memory: 116.8 MB (46.37%) - LeetSync

--- 4183-count-caesar-cipher-pairs/count-caesar-cipher-pairs.cpp
+++ 4183-count-caesar-cipher-pairs/count-caesar-cipher-pairs.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     long long countPairs(vector<string>& words) {
         int n = words.size();
-
-        unordered_map<int,string> word_to_sig;
-
-        for(int idx = 0; idx < n; idx++){
-            string a = words[idx];
-            string sig = "";
-            for(int i = 1; i < a.size(); i++){
-                sig += to_string((a[i] - a[0] + 26) % 26);
-            }
-            word_to_sig[idx] = sig;
-        }
-
         unordered_map<string,int> sig_freq;
         long long ans = 0;
 
         for (int i = 0; i < n; i++) {
-            ans += sig_freq[word_to_sig[i]];
-            sig_freq[word_to_sig[i]]++;
+            string sig_j = "";
+            for(int k = 1; k < words[i].size(); k++){
+                sig_j += to_string((words[i][k] - words[i][0] + 26)%26);
+            }
+
+            ans += sig_freq[sig_j];
+            sig_freq[sig_j]++;
         }
 
         
