3c3a279c28c4549678d91a7dfb473ccccc4347e5
Ritwik K <61523467+Ritwik7631@users.noreply.github.com>
2026-02-07 23:59:29 -0800

    Time: 495 ms (29.11%) | Memory: 300.8 MB (14.67%) - LeetSync

--- 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
+++ 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -1,43 +1,30 @@
-#include <bits/stdc++.h>
-using namespace std;
-
+using ll = long long;
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        int n = (int)nums.size();
-        deque<int> maxdq; // indices, nums decreasing -> front is max
-        deque<int> mindq; // indices, nums increasing -> front is min
-
-        long long ans = 0;
+        multiset<ll> ms;
+        ll ans = 0;
         int l = 0;
 
-        for (int r = 0; r < n; r++) {
-            // push r into maxdq
-            while (!maxdq.empty() && nums[maxdq.back()] <= nums[r]) maxdq.pop_back();
-            maxdq.push_back(r);
+        for(int r = 0; r <(int)nums.size(); r++){
+            ms.insert(nums[r]);
 
-            // push r into mindq
-            while (!mindq.empty() && nums[mindq.back()] >= nums[r]) mindq.pop_back();
-            mindq.push_back(r);
+            while(l <= r){
+                ll mn = *ms.begin();
+                ll mx = *ms.rbegin();
+                ll len = (r-l+1)*1LL;
 
-            // shrink until valid
-            while (l <= r) {
-                long long mx = nums[maxdq.front()];
-                long long mn = nums[mindq.front()];
-                long long len = (long long)r - l + 1;
-                __int128 cost = (__int128)(mx - mn) * (__int128)len;
+                ll cost = (mx-mn) * len;
 
-                if (cost <= k) break;
+                if(cost <= k) break;
 
-                // remove l if it falls out
-                if (!maxdq.empty() && maxdq.front() == l) maxdq.pop_front();
-                if (!mindq.empty() && mindq.front() == l) mindq.pop_front();
+                auto it = ms.find(nums[l]);
+                ms.erase(it);
                 l++;
             }
 
-            ans += (long long)r - l + 1;
+            ans += (ll)(r-l+1);
         }
-
         return ans;
     }
 };
